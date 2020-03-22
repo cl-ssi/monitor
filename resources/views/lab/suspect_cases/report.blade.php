df3f4c5bbd28aeb840c2e3ca5ab88a6f548290fd
AquaroTorres <alvarotorres@gmail.com>
2020-03-22 16:55:23 -0300

    Reporte ampliado

--- resources/views/lab/suspect_cases/report.blade.php
+++ resources/views/lab/suspect_cases/report.blade.php
@@ -10,6 +10,10 @@
         <table class="table table-bordered col3">
             <tbody>
                 <tr>
+                    <th>Total enviados a análisis</th>
+                    <td>{{ $cases->count() }}</td>
+                </tr>
+                <tr>
                     <td>Total positivos</td>
                     <td>{{ $cases->where('pscr_sars_cov_2','positive')->count() }}</td>
                 </tr>
@@ -21,9 +25,34 @@
                     <td>Total sin resultados</td>
                     <td>{{ $cases->where('pscr_sars_cov_2','')->count() }}</td>
                 </tr>
+
                 <tr>
-                    <th>Total enviados a análisis</th>
-                    <td>{{ $cases->count() }}</td>
+                    <td>Total hospitaliazados</td>
+                    <td>{{ $cases->where('status','Hospitalizado')->count() }}</td>
+                </tr>
+                <tr>
+                    <td>Total hospitaliazados negativos</td>
+                    <td>{{ $cases->where('status','Hospitalizado')->where('pscr_sars_cov_2','negative')->count() }}</td>
+                </tr>
+                <tr>
+                    <td>Total hospitaliazados positivos</td>
+                    <td>{{ $cases->where('status','Hospitalizado')->where('pscr_sars_cov_2','positive')->count() }}</td>
+                </tr>
+                <tr>
+                    <td>Total hospitaliazados sin resultados</td>
+                    <td>{{ $cases->where('status','Hospitalizado')->where('pscr_sars_cov_2','')->count() }}</td>
+                </tr>
+                <tr>
+                    <td>Total sospechas APS</td>
+                    <td>{{ $cases->whereIn('origin',['CESFAM Guzmán','Hector Reyno'])->count() }}</td>
+                </tr>
+                <tr>
+                    <td>Total sospechas Hospital</td>
+                    <td>{{ $cases->where('origin','Hospital ETG')->count() }}</td>
+                </tr>
+                <tr>
+                    <td>Total sospechas Privados</td>
+                    <td>{{ $cases->whereIn('origin',['Clínica Tarapacá','Clínica Iquique'])->count() }}</td>
                 </tr>
             </tbody>
         </table>
