7d5982a3d05a31406c0082e1fef1990f5686ee04
sick <sick_iqq@hotmail.com>
2020-03-26 19:27:55 -0600

    Map v2.0

--- resources/views/patients/georeferencing/georeferencing.blade.php
+++ resources/views/patients/georeferencing/georeferencing.blade.php
@@ -15,8 +15,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://js.api.here.com/v3/3.1/mapsjs-core.js" type="text/javascript" charset="utf-8"></script>
 <script src="https://js.api.here.com/v3/3.1/mapsjs-service.js" type="text/javascript" charset="utf-8"></script>
-<script src="https://js.hereapi.cn/v3/3.0/mapsjs-ui.js" type="text/javascript" charset="utf-8"></script>
-<link rel="stylesheet" type="text/css" href="https://js.hereapi.cn/v3/3.0/mapsjs-ui.css" />
+<script src="https://js.api.here.com/v3/3.1/mapsjs-ui.js" type="text/javascript" charset="utf-8"></script>
+<script src="https://js.api.here.com/v3/3.1/mapsjs-mapevents.js" type="text/javascript" ></script>
+<link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css" />
 
 <script type="text/javascript">
 $( document ).ready(function() {
@@ -26,75 +27,132 @@ var platform = new H.service.Platform({
   'apikey': '5mKawERqnzL1KMnNIt4n42gAV8eLomjQPKf5S5AAcZg'
 });
 
-// // Retrieve the target element for the map:
-// var targetElement = document.getElementById('mapContainer');
-//
-// // Get default map types from the platform object:
-// var defaultLayers = platform.createDefaultLayers();
-//
-// // Instantiate the map:
-// var map = new H.Map(
-//   document.getElementById('mapContainer'),
-//   defaultLayers.vector.normal.map,
-//   {
-//     zoom: 13,
-//     center: { lat: -20.213750, lng: -70.152506 }
-//   }
-// );
-//
-// @foreach($suspectCases as $key => $case)
-//   @if($case->pscr_sars_cov_2 == 'positive')
-//     @if($case->patient->demographic != null)
-//
-//       // Create the parameters for the geocoding request:
-//         var geocodingParams = {
-//             searchText: '{{$case->patient->demographic->address}}, {{$case->patient->demographic->commune}}, chile'
-//           };
-//
-//       // Define a callback function to process the geocoding response:
-//       var onResult = function(result) {
-//
-//         var locations = result.Response.View[0].Result,
-//             position,
-//             marker;
-//
-//         // Add a marker for each location found
-//         for (i = 0;  i < locations.length; i++) {
-//           position = {
-//             lat: locations[i].Location.DisplayPosition.Latitude,
-//             lng: locations[i].Location.DisplayPosition.Longitude
-//           };
-//           marker = new H.map.Marker(position);
-//           map.addObject(marker);
-//         }
-//
-//       };
-//
-//       // Get an instance of the geocoding service:
-//       var geocoder = platform.getGeocodingService();
-//
-//       // Error
-//       geocoder.geocode(geocodingParams, onResult, function(e) {
-//         alert(e);
-//       });
-//     @endif
-//   @endif
-// @endforeach
+// Retrieve the target element for the map:
+var targetElement = document.getElementById('mapContainer');
 
-// Obtain the default map types from the platform
- var maptypes = platform.createDefaultLayers();
+// Get default map types from the platform object:
+var defaultLayers = platform.createDefaultLayers();
 
- // Instantiate and display a map
- var map = new H.Map(document.getElementById('mapContainer'), maptypes.vector.normal.map, {
-   center: {lat: -20, lng: -70},
-   zoom: 8
- });
+// Instantiate the map:
+var map = new H.Map(
+  document.getElementById('mapContainer'),
+  defaultLayers.vector.normal.map,
+  {
+    zoom: 12.7,
+    center: { lat: -20.25, lng: -70.1 }
+  }
+);
 
- // Enable the event system on the map instance:
-var mapEvents = new H.mapevents.MapEvents(map);
+// Create the default UI:
+var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+var ui = H.ui.UI.createDefault(map, defaultLayers, 'es-ES');
+var mapSettings = ui.getControl('mapsettings');
+var zoom = ui.getControl('zoom');
+var scalebar = ui.getControl('scalebar');
 
-// Instantiate the default behavior, providing the mapEvents object:
-new H.mapevents.Behavior(mapEvents);
+//mapSettings.setAlignment('top-left');
+//zoom.setAlignment('top-left');
+//scalebar.setAlignment('top-left');
+
+@foreach($suspectCases as $key => $case)
+  @if($case->pscr_sars_cov_2 == 'positive' || $case->pscr_sars_cov_2 == 'pending')
+    @if($case->patient->demographic != null)
+
+      // Define a variable holding SVG mark-up that defines an icon image:
+      var svgMarkupBlue = '<svg width="10" height="10" ' +
+        'xmlns="http://www.w3.org/2000/svg">' +
+        '<rect stroke="white" fill="blue" x="1" y="1" width="22" ' +
+        'height="22" /><text x="12" y="18" font-size="12pt" ' +
+        'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+        'fill="white"></text></svg>';
+
+      var svgMarkupRed = '<svg width="10" height="10" ' +
+        'xmlns="http://www.w3.org/2000/svg">' +
+        '<rect stroke="white" fill="red" x="1" y="1" width="22" ' +
+        'height="22" /><text x="12" y="18" font-size="12pt" ' +
+        'font-family="Arial" font-weight="bold" text-anchor="middle" ' +
+        'fill="white"></text></svg>';
+
+      var iconBlue = new H.map.Icon(svgMarkupBlue);
+      var iconRed = new H.map.Icon(svgMarkupRed);
+
+      // Create the parameters for the geocoding request:
+        var geocodingParams = {
+            searchText: '{{$case->patient->demographic->address}}, {{$case->patient->demographic->commune}}, chile'
+          };
+
+      // Define a callback function to process the geocoding response:
+      var onResult = function(result) {
+
+        var locations = result.Response.View[0].Result,
+            position,
+            marker;
+
+        // Add a marker for each location found
+        for (i = 0;  i < locations.length; i++) {
+          position = {
+            lat: locations[i].Location.DisplayPosition.Latitude,
+            lng: locations[i].Location.DisplayPosition.Longitude
+          };
+
+          @if($case->pscr_sars_cov_2 == 'positive')
+            marker = new H.map.Marker(position, {icon: iconRed});
+          @else
+            marker = new H.map.Marker(position, {icon: iconBlue});
+          @endif
+
+
+          map.addObject(marker);
+        }
+
+      };
+
+      // Get an instance of the geocoding service:
+      var geocoder = platform.getGeocodingService();
+
+      // Error
+      geocoder.geocode(geocodingParams, onResult, function(e) {
+        alert(e);
+      });
+
+      //panTheMap(map);
+    @endif
+  @endif
+@endforeach
+
+
+function panTheMap(map) {
+  var viewPort,
+      incX = 1,
+      incY = 2,
+      x = 100,
+      y = 100;
+
+  // Obtain the view port object of the map to manipulate its screen coordinates
+  var viewPort = map.getViewPort(),
+      // function calculates new screen coordinates and calls
+      // viewport's interaction method with them
+      pan = function() {
+        x = x + incX;
+        if (Math.abs(x) > 100) {
+          incX = -incX;
+        }
+
+        y = y + incY;
+        if (Math.abs(y) > 100) {
+          incY = -incY;
+        }
+
+        viewPort.interaction(x, y);
+      };
+
+  // set interaction modifier that provides information which map properties
+  // change with each "interact" call
+  viewPort.startInteraction(H.map.render.RenderEngine.InteractionModifiers.COORD, 0, 0);
+  // set up simple animation loop
+  setInterval(pan, 15);
+}
+
 
 
 
