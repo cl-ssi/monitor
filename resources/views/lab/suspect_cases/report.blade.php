8612a9307840e81e069bb2dac3904998297d3f3f
AquaroTorres <alvarotorres@gmail.com>
2020-04-01 15:59:09 -0300

    Report by age

--- resources/views/lab/suspect_cases/report.blade.php
+++ resources/views/lab/suspect_cases/report.blade.php
@@ -354,6 +354,287 @@
 
             </tbody>
         </table>
+
+
+        <table class="table table-sm table-bordered">
+            <thead>
+                <tr class="table-active">
+                    <th>Rango Edad</th>
+                    <th>Positivo</th>
+                    <th>Pendiente</th>
+                    <th>Negativo</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="text-center">
+                    <td>0-9</td>
+                    <td class="text-danger">
+                        {{
+                            $cases->where('pscr_sars_cov_2','positive')
+                                ->whereBetween('age',[0,9])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','pending')
+                                ->whereBetween('age',[0,9])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','negative')
+                                ->whereBetween('age',[0,9])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                </tr>
+                <tr class="text-center">
+                    <td>10-19</td>
+                    <td class="text-danger">
+                        {{
+                            $cases->where('pscr_sars_cov_2','positive')
+                                ->whereBetween('age',[10,19])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','pending')
+                                ->whereBetween('age',[10,19])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','negative')
+                                ->whereBetween('age',[10,19])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                </tr>
+                <tr class="text-center">
+                    <td>20-29</td>
+                    <td class="text-danger">
+                        {{
+                            $cases->where('pscr_sars_cov_2','positive')
+                                ->whereBetween('age',[20,29])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','pending')
+                                ->whereBetween('age',[20,29])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','negative')
+                                ->whereBetween('age',[20,29])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                </tr>
+                <tr class="text-center">
+                    <td>30-39</td>
+                    <td class="text-danger">
+                        {{
+                            $cases->where('pscr_sars_cov_2','positive')
+                                ->whereBetween('age',[30,39])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','pending')
+                                ->whereBetween('age',[30,39])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','negative')
+                                ->whereBetween('age',[30,39])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                </tr>
+                <tr class="text-center">
+                    <td>40-49</td>
+                    <td class="text-danger">
+                        {{
+                            $cases->where('pscr_sars_cov_2','positive')
+                                ->whereBetween('age',[40,49])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','pending')
+                                ->whereBetween('age',[40,49])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','negative')
+                                ->whereBetween('age',[40,49])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                </tr>
+                <tr class="text-center">
+                    <td>50-59</td>
+                    <td class="text-danger">
+                        {{
+                            $cases->where('pscr_sars_cov_2','positive')
+                                ->whereBetween('age',[50,59])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','pending')
+                                ->whereBetween('age',[50,59])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','negative')
+                                ->whereBetween('age',[50,59])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                </tr>
+                <tr class="text-center">
+                    <td>60-69</td>
+                    <td class="text-danger">
+                        {{
+                            $cases->where('pscr_sars_cov_2','positive')
+                                ->whereBetween('age',[60,69])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','pending')
+                                ->whereBetween('age',[60,69])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','negative')
+                                ->whereBetween('age',[60,69])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                </tr>
+                <tr class="text-center">
+                    <td>70-79</td>
+                    <td class="text-danger">
+                        {{
+                            $cases->where('pscr_sars_cov_2','positive')
+                                ->whereBetween('age',[70,79])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','pending')
+                                ->whereBetween('age',[70,79])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','negative')
+                                ->whereBetween('age',[70,79])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                </tr>
+                <tr class="text-center">
+                    <td>80-></td>
+                    <td class="text-danger">
+                        {{
+                            $cases->where('pscr_sars_cov_2','positive')
+                                ->whereBetween('age',[80,900])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','pending')
+                                ->whereBetween('age',[80,900])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','negative')
+                                ->whereBetween('age',[80,900])
+                                ->whereNotNull('age')
+                                ->count()
+                        }}
+                    </td>
+                </tr>
+                <tr class="text-center">
+                    <td>S/R</td>
+                    <td class="text-danger">
+                        {{
+                            $cases->where('pscr_sars_cov_2','positive')
+                                ->whereNull('age')
+                                ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','pending')
+                            ->whereNull('age')
+                            ->count()
+                        }}
+                    </td>
+                    <td>
+                        {{
+                            $cases->where('pscr_sars_cov_2','negative')
+                            ->whereNull('age')
+                            ->count()
+                        }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
 
