f52629c43e76bb97d7a319812c7ef9500966aba2
sick <sick_iqq@hotmail.com>
2020-04-07 15:44:38 -0600

    endpoint: se agrega ruta para enviar data desde intranet (pruebas)

---
+++ app/Http/Controllers/EndpointController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EndpointController extends Controller
+{
+  public function receiveDispatchC19(Request $request)
+  {
+    dd($request);
+  }
+}
