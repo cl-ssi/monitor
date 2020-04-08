1a739f26cfe5e7fd846eb04e9c4d0b773ee7ae64
AquaroTorres <alvarotorres@gmail.com>
2020-04-08 12:23:02 -0400

    Sólo cambio de tabulacion

--- app/Http/Controllers/EndpointController.php
+++ app/Http/Controllers/EndpointController.php
@@ -7,13 +7,13 @@ use App\Pharmacies\Dispatch;
 
 class EndpointController extends Controller
 {
-  public function receiveDispatchC19(Request $request)
-  {
-    dd("holaaaa", $request->dispatch);
-    $dispatch = new Dispatch();
-    $dispatch->fill($request->dispatch);
-    $dispatch->save();
+    public function receiveDispatchC19(Request $request)
+    {
+        dd("holaaaa", $request->dispatch);
+        $dispatch = new Dispatch();
+        $dispatch->fill($request->dispatch);
+        $dispatch->save();
 
-    //return view('welcome');
-  }
+        //return view('welcome');
+    }
 }
