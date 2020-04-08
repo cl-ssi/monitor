279a2941ba6db2bd8cdbe0971381f5b10de2df7e
AquaroTorres <alvarotorres@gmail.com>
2020-04-08 12:22:22 -0400

    change password

---
+++ resources/views/users/change_password.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title', 'Cambiar clave')
+
+@section('content')
+<h3 class="mb-3">Cambiar clave</h3>
+
+<form method="POST" class="form-horizontal" action="{{ route('users.password.update') }}">
+    @csrf
+    @method('PUT')
+
+    <div class="form-row">
+        <fieldset class="form-group col">
+            <label for="for_current_password">Password Actual</label>
+            <input type="password" class="form-control" name="current_password"
+                id="for_current_password" required>
+        </fieldset>
+
+        <fieldset class="form-group col">
+            <label for="for_new_password">Nuevo Password</label>
+            <input type="password" class="form-control" name="new_password"
+                id="for_new_password" required>
+        </fieldset>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Cambiar</button>
+
+</form>
+
+@endsection
+
+@section('custom_js')
+
+@endsection
