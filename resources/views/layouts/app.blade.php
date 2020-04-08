e5930655649c116493e2b8bff04b425fac550d1b
AquaroTorres <alvarotorres@gmail.com>
2020-04-08 12:22:22 -0400

    change password

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,6 +107,9 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('users.password.show_form') }}">
+                                        Cambiar clave
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
