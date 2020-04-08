30dc707b0e282595233a1bbc9dceb319e52604c7
AquaroTorres <alvarotorres@gmail.com>
2020-04-08 03:12:25 -0400

    Demographic re orden

--- resources/views/patients/demographic/create.blade.php
+++ resources/views/patients/demographic/create.blade.php
@@ -1,6 +1,6 @@
 <div class="form-row">
 
-  <fieldset class="form-group col-md">
+  <fieldset class="form-group col-12 col-md-2">
       <label for="for_street_type">Vìa de residencia</label>
       <select name="street_type" id="for_street_type" class="form-control">
           <option value="Calle">Calle</option>
@@ -10,54 +10,46 @@
       </select>
   </fieldset>
 
-</div>
 
-<div class="form-row">
-
-    <fieldset class="form-group col-4">
+    <fieldset class="form-group col-12 col-md-4">
         <label for="for_address">Dirección</label>
         <input type="text" class="form-control" name="address" id="for_address">
     </fieldset>
 
-    <fieldset class="form-group col-4">
+    <fieldset class="form-group col-6 col-md-2">
         <label for="for_number">Número</label>
         <input type="text" class="form-control" name="number" id="for_number">
     </fieldset>
 
-    <fieldset class="form-group col-4">
-        <label for="for_deparment">Departamento</label>
+    <fieldset class="form-group col-6 col-md-1">
+        <label for="for_deparment">Depto.</label>
         <input type="text" class="form-control" name="deparment" id="for_deparment">
     </fieldset>
 
+    <fieldset class="form-group col-12 col-md-3">
+        <label for="for_town">Población/Villa</label>
+        <input type="text" class="form-control" name="town" id="for_town">
+    </fieldset>
 </div>
 
 <div class="form-row">
 
-  <fieldset class="form-group col-4">
+  <fieldset class="form-group col-6 col-md-4">
       <label for="regiones">Región</label>
       <select class="form-control" name="region" id="regiones"></select>
   </fieldset>
 
-  <fieldset class="form-group col-4">
+  <fieldset class="form-group col-6 col-md-4">
       <label for="comunas">Comuna</label>
       <select class="form-control" name="commune" id="comunas"></select>
   </fieldset>
 
-  <fieldset class="form-group col-4">
-      <label for="for_town">Población/Villa</label>
-      <input type="text" class="form-control" name="town" id="for_town">
-  </fieldset>
-
-</div>
-
-<div class="form-row">
-
-    <fieldset class="form-group col-6">
+    <fieldset class="form-group col-6 col-md-2">
         <label for="for_latitude">Latitud</label>
         <input type="number" step="00.00000001" class="form-control" name="latitude" id="for_latitude">
     </fieldset>
 
-    <fieldset class="form-group col-6">
+    <fieldset class="form-group col-6 col-md-2">
         <label for="for_longitude">Longitud</label>
         <input type="number" step="00.00000001" class="form-control" name="longitude" id="for_longitude">
     </fieldset>
@@ -66,12 +58,12 @@
 
 <div class="form-row">
 
-    <fieldset class="form-group col-8">
+    <fieldset class="form-group col-12 col-md-8">
         <label for="for_email">email</label>
         <input type="email" class="form-control" name="email" id="for_email">
     </fieldset>
 
-    <fieldset class="form-group col-4">
+    <fieldset class="form-group col-12 col-md-4">
         <label for="for_telephone">Teléfono</label>
         <input type="text" class="form-control" name="telephone" id="for_telephone">
     </fieldset>
