fe5942c52ee57047d2dc7a122c9910a0a9a362e7
AquaroTorres <alvarotorres@gmail.com>
2020-04-12 21:16:13 -0400

    Add laboratory model

---
+++ app/Http/Controllers/LaboratoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Laboratory;
+use Illuminate\Http\Request;
+
+class LaboratoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Laboratory  $laboratory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Laboratory $laboratory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Laboratory  $laboratory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Laboratory $laboratory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Laboratory  $laboratory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Laboratory $laboratory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Laboratory  $laboratory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Laboratory $laboratory)
+    {
+        //
+    }
+}
