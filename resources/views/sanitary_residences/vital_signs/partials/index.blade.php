8ad3b03b782a9e68558c7e0b9f7a620b79755c05
AquaroTorres <alvarotorres@gmail.com>
2020-04-14 18:11:13 -0400

    Versión ajustada a celular

--- resources/views/sanitary_residences/vital_signs/partials/index.blade.php
+++ resources/views/sanitary_residences/vital_signs/partials/index.blade.php
@@ -4,15 +4,15 @@
     <thead>
         <tr>
             <th>Fecha y hora</th>
-            <th>Chequeado por</th>
-            <th>Temperatura</th>
-            <th>Frec. Cardiaca</th>
-            <th>Presión Arterial</th>
-            <th>Frec. Respiratoria</th>
+            <th>Temp.</th>
+            <th>Frec. Card.</th>
+            <th>P. Arterial</th>
+            <th>Frec. Resp.</th>
             <th>SAT 02</th>
             <th>HGT</th>
             <th>Escala Dolor</th>
             <th>Observaciones</th>
+            <th>Funcionario</th>
             <th></th>
         </tr>
     </thead>
@@ -20,23 +20,21 @@
         @foreach($booking->vitalSigns->reverse() as $vitalsign)
         <tr>
             <td nowrap>{{ $vitalsign->created_at->format('d-m-Y H:i') }}</td>
-            <td nowrap>{{ $vitalsign->user->name }}</td>
-            <td>{{ $vitalsign->temperature }}</td>
-            <td>{{ $vitalsign->heart_rate }}</td>
-            <td>{{ $vitalsign->blood_pressure }}</td>
-            <td>{{ $vitalsign->respiratory_rate }}</td>
-            <td>{{ $vitalsign->oxygen_saturation }}</td>
-            <td>{{ $vitalsign->hgt }}</td>
-            <td>{{ $vitalsign->pain_scale }}</td>
+            <td class="text-center">{{ $vitalsign->temperature }}</td>
+            <td class="text-center">{{ $vitalsign->heart_rate }}</td>
+            <td class="text-center">{{ $vitalsign->blood_pressure }}</td>
+            <td class="text-center">{{ $vitalsign->respiratory_rate }}</td>
+            <td class="text-center">{{ $vitalsign->oxygen_saturation }}</td>
+            <td class="text-center">{{ $vitalsign->hgt }}</td>
+            <td class="text-center">{{ $vitalsign->pain_scale }}</td>
             <td>{{ $vitalsign->observations }}</td>
-
-            <td><button type="submit" class="btn btn-outline-secondary btn-sm" id="btn_{{$vitalsign->id}}">
-    							<i class="fas fa-edit"></i>
-    						</button>
+            <td nowrap>{{ $vitalsign->user->name }}</td>
+            <td>
+                <button type="submit" class="btn btn-outline-secondary btn-sm" id="btn_{{$vitalsign->id}}">
+    				<i class="fas fa-edit"></i>
+    			</button>
             </td>
-
         </tr>
         @endforeach
-
     </tbody>
 </table>
