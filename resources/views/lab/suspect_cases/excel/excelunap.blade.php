7f3821f1ab9c20057d8e6f59d7d0df71842c62c9
Esteban <tebiccr@gmail.com>
2020-04-16 23:43:27 -0400

    excel casos sospechosos laboratorio unap

---
+++ resources/views/lab/suspect_cases/excel/excelunap.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+@section('title', 'Excel Unap')
+
+@section('content')
+
+
+
+<a class="btn btn-outline-success btn-sm mb-3" id="downloadLink" onclick="exportF(this)">Descargar en excel</a>
+
+<table class="table table-sm table-bordered table-responsive small" id="tabla_booking">
+    <thead>
+        <th nowrap>RUN</th>
+        <th nowrap>Nombre</th>
+        <th nowrap>Sexo</th>
+        <th nowrap>Edad</th>
+        <th nowrap>Tipo muestra</th>
+        <th nowrap>Resultado</th>
+        <th nowrap>Fecha de toma de muestra</th>
+        <th nowrap>Fecha de recepción de la muestra</th>
+        <th nowrap>Fecha de resultado</th>
+        <th nowrap>Hospital o establecimiento de origen (lugar donde se toma la muestra)</th>
+        <th nowrap>Región de establecimiento de origen</th>
+        <th nowrap>Laboratorio de referencia (lugar donde se procesa la muestra)</th>
+        <th nowrap>Región de laboratorio donde se procesa la muestra</th>
+        <th nowrap>Teléfono de contacto de paciente</th>
+        <th nowrap>Correo de contacto de paciente</th>
+        <th nowrap>Dirección de contacto de paciente</th>        
+    </thead>
+    <tbody>
+        @foreach ($cases as $case)
+        <tr>
+            <td nowrap>{{$case->patient->identifier}}</td>
+            <td nowrap>{{$case->patient->fullName }}</td>
+            <td nowrap>{{strtoupper($case->patient->genderEsp)}}</td>
+            <td nowrap>{{$case->age }}</td>
+            <td nowrap>SARS-CoV-2</td>
+            <td nowrap>{{ $case->covid19 }}</td>
+            <td nowrap>{{ $case->sample_at->format('d-m-Y') }}</td>
+            <td nowrap>{{ ($case->created_at)? $case->created_at->format('d-m-Y'): '' }}</td>
+            <td nowrap>{{ ($case->pscr_sars_cov_2_at)? $case->pscr_sars_cov_2_at->format('d-m-Y'): '' }}</td>
+            <td nowrap>{{strtoupper($case->origin)}}</td>
+            <td nowrap>TARAPACÁ</td>
+            <td nowrap>UNAPSSI</td>
+            <td nowrap>TARAPACÁ</td>
+            <td nowrap>{{ ($case->patient->demographic)?$case->patient->demographic->telephone:'' }}</td>
+            <td nowrap>{{ ($case->patient->demographic)?$case->patient->demographic->email:'' }}</td>
+            <td nowrap>{{ ($case->patient->demographic)?$case->patient->demographic->address:'' }} {{ ($case->patient->demographic)? $case->patient->demographic->number:'' }} {{ ($case->patient->demographic)?$case->patient->demographic->commune:'' }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+    
+
+</table>
+
+
+@endsection
+
+@section('custom_js')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script type="text/javascript">
+function exportF(elem) {
+    var table = document.getElementById("tabla_booking");
+    var html = table.outerHTML;
+    var html_no_links = html.replace(/<a[^>]*>|<\/a>/g, "");//remove if u want links in your table
+    var url = 'data:application/vnd.ms-excel,' + escape(html_no_links); // Set your html table into url
+    elem.setAttribute("href", url);
+    elem.setAttribute("download", "casos_sospechosos_unap.xls"); // Choose the file name
+    return false;
+}
+
+</script>
+@endsection
