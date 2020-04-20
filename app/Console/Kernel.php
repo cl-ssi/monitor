db8dc6ef3a70833f1ce658523746d5e0a5b07c89
sick <sick_iqq@hotmail.com>
2020-04-20 13:04:05 -0600

    test ->dailyAt('13:00');

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('Report:backup')->everyMinute();
+        $schedule->command('Report:backup')->dailyAt('15:05');
     }
 
     /**
