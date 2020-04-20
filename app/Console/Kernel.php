ed0944d88dc252cb05a1f61a3d778ac8ae9578ec
sick <sick_iqq@hotmail.com>
2020-04-20 08:07:09 -0600

    file_report: se incorporó descarga en excel report histórico: se agregó nuevo command para guardar collection.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        'App\Console\Commands\ReportBackup',
     ];
 
     /**
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('Report:backup')->everyMinute();
     }
 
     /**
