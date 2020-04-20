e1af1a8d5fe98aeef89637de62bb7c4cc9007c8f
Esteban <tebiccr@gmail.com>
2020-04-20 02:50:34 -0400

    pueden editar solamente los propietarios

--- resources/views/sanitary_residences/indications/partials/index.blade.php
+++ resources/views/sanitary_residences/indications/partials/index.blade.php
@@ -9,7 +9,7 @@
         </footer>
     </div>
     <div class="col-2 col-md-1">
-        @if($indicaciones->created_at->diff(now())->days < 1)
+        @if($indicaciones->created_at->diff(now())->days < 1 and $indicaciones->user->id == Auth::id())
             <button type="submit" class="btn btn-outline-secondary btn-sm" id="btn_indications_{{$indicaciones->id}}">
                 <i class="fas fa-edit"></i>
         @endif
