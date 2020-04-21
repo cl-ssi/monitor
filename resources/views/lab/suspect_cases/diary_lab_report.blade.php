e5b7ea4c5093cbab003b7efafb1e8534185e7d52
sick <sick_iqq@hotmail.com>
2020-04-21 17:56:37 -0600

    Reporte diario laboratorio

---
+++ resources/views/lab/suspect_cases/diary_lab_report.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('title', 'Reporte COVID-19')
+
+@section('content')
+<h3 class="mb-3">Cantidad toma de muestras diarias</h3>
+
+<table class="table table-sm table-bordered">
+    <thead>
+        <tr class="text-center">
+            <th>Día</th>
+            <th>Cant.Exámenes</th>
+        </tr>
+    </thead>
+    <tbody>
+      @foreach($total_muestras_diarias as $muestra_diaria)
+        <tr>
+            <td>{{ Carbon\Carbon::parse($muestra_diaria->sample_at)->format('Y-m-d') }}</td>
+            <td>{{ $muestra_diaria->total }}</td>
+        </tr>
+      @endforeach
+    </tbody>
+</table>
+
+<br /><hr />
+
+<h3 class="mb-3">Cantidad exámenes realizados por laboratorios</h3>
+
+<table class="table table-sm table-bordered">
+    <thead>
+        <tr class="text-center">
+            <th>Día</th>
+            @foreach($total_muestras_x_lab_columnas as $key => $muestra_x_lab_columna)
+              <th>{{$key}}</th>
+            @endforeach
+        </tr>
+    </thead>
+    <tbody>
+
+        @foreach($total_muestras_x_lab_filas as $key => $muestra_x_lab_filas)
+          <tr>
+            <td>{{Carbon\Carbon::parse($key)->format('Y-m-d')}}</td>
+            @foreach($total_muestras_x_lab_columnas as $key2 => $muestra_x_lab_columna)
+                <td>
+                  @foreach ($muestra_x_lab_filas as $key3 => $data)
+                    @if($key2 == $key3)
+                      {{$data['cantidad']}}
+                    @endif
+                  @endforeach
+                </td>
+            @endforeach
+          </tr>
+        @endforeach
+      {{-- @endforeach --}}
+    </tbody>
+</table>
+
+@endsection
+
+@section('custom_js_head')
+<script type="text/javascript">
+
+</script>
+@endsection
