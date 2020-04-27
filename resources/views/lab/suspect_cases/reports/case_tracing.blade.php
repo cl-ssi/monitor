577eb1959e46fcd346196e4e1b6ec3fa845c62a1
AcuaroTorres <alvaro.torres@gmail.com>
2020-04-27 04:52:57 -0400

    Beautifier case tracing

--- resources/views/lab/suspect_cases/reports/case_tracing.blade.php
+++ resources/views/lab/suspect_cases/reports/case_tracing.blade.php
@@ -11,25 +11,21 @@
 <table class="table table-sm table-bordered table-responsive small" >
     <thead>
         <tr class="text-center">
-          <th colspan="6"></th>
-          <th colspan="1"></th>
-          <th colspan="2"></th>
-          <th colspan="2">Covid</th>
-          <th colspan="2">IFD</th>
-          <th colspan="2">Laboratorio</th>
-          <th colspan="2" nowrap>Entrega de resultados</th>
-          <th colspan="5"></th>
-
-          @for ($i=0; $i < ($cont_casos-1); $i++)
-            <th colspan="3" nowrap>Covid salida {{$i+1}}</th>
-          @endfor
-
-          <th colspan="2"></th>
-
+            <th colspan="6"></th>
+            <th colspan="1"></th>
+            <th colspan="2"></th>
+            <th colspan="2">Covid</th>
+            <th colspan="2">IFD</th>
+            <th colspan="2">Laboratorio</th>
+            <th colspan="2" nowrap>Entrega de resultados</th>
+            <th colspan="5"></th>
+            @for ($i=0; $i < ($cont_casos-1); $i++)
+                <th colspan="3" nowrap>Covid salida {{$i+1}}</th>
+            @endfor
+            <th colspan="2"></th>
         </tr>
         <tr class="text-center">
             <th>°</th>
-            <th>Id</th>
             <th>Paciente</th>
             <th>Run</th>
             <th>Edad</th>
@@ -47,8 +43,8 @@
             <th nowrap>Fecha IFD</th>
             <th>IFD</th>
 
+            <th>Fecha envío</th>
             <th>Laboratorio</th>
-            <th>Fecha</th>
 
             <th>Fecha</th>
             <th>Mecanismo</th>
@@ -60,9 +56,9 @@
             <th nowrap>Contacto directo</th>
 
             @for ($i=0; $i < ($cont_casos-1); $i++)
-              <th nowrap>Fecha Muestra</th>
-              <th nowrap>Fecha Resultado</th>
-              <th>Covid</th>
+                <th nowrap>Fecha Muestra</th>
+                <th nowrap>Fecha Resultado</th>
+                <th>Covid</th>
             @endfor
 
             <th nowrap>Fecha Alta</th>
@@ -71,79 +67,70 @@
     </thead>
     <tbody>
         @php $ct=1 @endphp
-      @foreach($patients as $patient)
-        @php
-          $suspectCase = $patient->suspectCases->where('pscr_sars_cov_2', 'positive')->first();
-        @endphp
-        <tr>
-            <td>{{ $ct++ }}</td>
-            <td nowrap>{{ $patient->id }}</td>
-            <td nowrap>
-                <a href="{{ route('patients.edit', $patient) }}">{{ $patient->fullName }}</a>
-            </td>
-            <td nowrap>{{ $patient->identifier }}</td>
-            <td nowrap>{{ $patient->suspectCases->last()->age }}</td>
-            <td nowrap>{{ strtoupper($patient->genderEsp) }}</td>
-            <td nowrap>{{ ($patient->demographic) ? $patient->demographic->commune : '' }}</td>
-            <td nowrap>{{ $suspectCase->status }}</td>
+        @foreach($patients as $patient)
+            @php
+                $suspectCase = $patient->suspectCases->first();
+            @endphp
+            <tr>
+                <td>{{ $ct++ }}</td>
+                <td nowrap>
+                    <a href="{{ route('patients.edit', $patient) }}">{{ $patient->fullName }}</a>
+                </td>
+                <td nowrap>{{ $patient->identifier }}</td>
+                <td nowrap>{{ $patient->suspectCases->last()->age }}</td>
+                <td nowrap>{{ strtoupper($patient->genderEsp) }}</td>
+                <td nowrap>{{ ($patient->demographic) ? $patient->demographic->commune : '' }}</td>
+                <td nowrap>{{ $suspectCase->status }}</td>
 
-            <td nowrap>{{ $suspectCase->sample_at->format('Y-m-d') }}</td>
-            <td nowrap>{{ $suspectCase->origin }}</td>
+                <td nowrap>{{ $suspectCase->sample_at->format('Y-m-d') }}</td>
+                <td nowrap>{{ $suspectCase->origin }}</td>
 
-            <td nowrap>{{ ($suspectCase->pscr_sars_cov_2_at) ? $suspectCase->pscr_sars_cov_2_at->format('Y-m-d') : '' }}</td>
-            <td nowrap>
-                <a href="{{ route('lab.suspect_cases.edit', $suspectCase) }}">{{ $suspectCase->covid19 }}</a>
-            </td>
+                <td nowrap>{{ ($suspectCase->pscr_sars_cov_2_at) ? $suspectCase->pscr_sars_cov_2_at->format('Y-m-d') : '' }}</td>
+                <td nowrap>
+                    <a href="{{ route('lab.suspect_cases.edit', $suspectCase) }}">{{ $suspectCase->covid19 }}</a>
+                </td>
 
-            <td nowrap>{{ ($suspectCase->result_ifd_at) ? $suspectCase->result_ifd_at->format('Y-m-d') : '' }}</td>
-            <td nowrap>{{ $suspectCase->result_ifd }}</td>
+                <td nowrap>{{ ($suspectCase->result_ifd_at) ? $suspectCase->result_ifd_at->format('Y-m-d') : '' }}</td>
+                <td nowrap>{{ $suspectCase->result_ifd }}</td>
 
-            <td nowrap>{{ $suspectCase->procesingLab }}</td>
+                <td nowrap>{{ ($suspectCase->sent_isp_at) ? $suspectCase->sent_isp_at->format('Y-m-d') : '' }}</td>
+                <td nowrap>{{ $suspectCase->procesingLab }}</td>
 
-            <td nowrap>{{ ($suspectCase->sent_isp_at) ? $suspectCase->sent_isp_at->format('Y-m-d') : '' }}</td>
+                <td nowrap>{{ ($suspectCase->notification_at) ? $suspectCase->notification_at->format('Y-m-d') : '' }}</td>
+                <td nowrap>{{ $suspectCase->notification_mechanism }}</td>
 
-            <td nowrap>{{ ($suspectCase->notification_at) ? $suspectCase->notification_at->format('Y-m-d') : '' }}</td>
-            <td nowrap>{{ $suspectCase->notification_mechanism }}</td>
+                <td nowrap>{{ $suspectCase->epidemiological_week }}</td>
+                <td nowrap>{{ $suspectCase->epivigila }}</td>
+                <td nowrap>{{ $suspectCase->paho_flu }}</td>
+                <td nowrap>{{ ($suspectCase->gestation == 1) ? 'Sí' : '' }}</td>
+                <td nowrap>{{ ($suspectCase->close_contact == 1) ? 'Sí':'' }}</td>
 
-            <td nowrap>{{ $suspectCase->epidemiological_week }}</td>
-            <td nowrap>{{ $suspectCase->epivigila }}</td>
-            <td nowrap>{{ $suspectCase->paho_flu }}</td>
-            <td nowrap>{{ ($suspectCase->gestation == 1) ? 'Sí' : '' }}</td>
-            <td nowrap>{{ ($suspectCase->close_contact == 1) ? 'Sí':'' }}</td>
-
-            @if($patient->suspectCases->count() == 1)
-              <td></td>
-              <td></td>
-              <td></td>
-            @else
-              @foreach ($patient->suspectCases as $key => $suspectCase2)
-                @if($key <> 0) {{-- no se imprime el primer item --}}
-                  <td nowrap>{{ $suspectCase2->sample_at->format('Y-m-d') }}</td>
-                  <td nowrap>
-                      {{ ($suspectCase2->pscr_sars_cov_2_at)? $suspectCase2->pscr_sars_cov_2_at->format('Y-m-d') : '' }}
-                  </td>
-                  <td nowrap>
-                      <a href="{{ route('lab.suspect_cases.edit', $suspectCase2) }}">{{ $suspectCase2->covid19 }}</a>
-                       {{ ($suspectCase2->discharge_test)? '' : 'x' }}
-                  </td>
+                @if($patient->suspectCases->count() == 1)
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                @else
+                    @foreach ($patient->suspectCases as $key => $suspectCase2)
+                        @if($key <> 0) {{-- no se imprime el primer item --}}
+                            <td nowrap>{{ $suspectCase2->sample_at->format('Y-m-d') }}</td>
+                            <td nowrap>
+                                {{ ($suspectCase2->pscr_sars_cov_2_at)? $suspectCase2->pscr_sars_cov_2_at->format('Y-m-d') : '' }}
+                            </td>
+                            <td nowrap>
+                                <a href="{{ route('lab.suspect_cases.edit', $suspectCase2) }}">{{ $suspectCase2->covid19 }}</a>
+                                {{ ($suspectCase2->discharge_test)? '' : 'x' }}
+                            </td>
+                        @endif
+                    @endforeach
                 @endif
-              @endforeach
-            @endif
 
-            <td nowrap>{{ $suspectCase->discharged_at }}</td>
-            <td nowrap>{{ $suspectCase->observation }}</td>
+                <td nowrap>{{ $suspectCase->discharged_at }}</td>
+                <td nowrap>{{ $suspectCase->observation }}</td>
 
-        </tr>
-      @endforeach
-
-      {{-- <tr>
-        <td><b>Total</b></td>
-        <td><b>{{$total_muestras_diarias->sum('total')}}</b></td>
-      </tr> --}}
+            </tr>
+        @endforeach
     </tbody>
 </table>
-
-
 
 @endsection
 
