c8d7ffcc6569e311fdeb114177ada015511aa759
Esteban <tebiccr@gmail.com>
2020-04-28 10:59:05 -0400

    correlativo loop excellall

--- resources/views/sanitary_residences/bookings/excel/excelall.blade.php
+++ resources/views/sanitary_residences/bookings/excel/excelall.blade.php
@@ -42,7 +42,7 @@
         @foreach ($bookings as $booking)
         @if ($booking->status == 'Residencia Sanitaria' and $booking->patient->suspectCases->last()->status == 'Residencia Sanitaria')
         <tr>
-            <td nowrap>{{$booking->id}}</td>
+            <td nowrap>{{$loop->iteration}}</td>
             <td nowrap>Iquique</td>
             <td nowrap>Iquique</td>
             <td nowrap>{{$booking->room->residence->name}}</td>
