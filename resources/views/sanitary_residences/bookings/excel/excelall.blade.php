477f8b55970329dc2400c66d9b6cd2225cad8f64
Esteban <tebiccr@gmail.com>
2020-04-28 11:09:12 -0400

    loop remaining

--- resources/views/sanitary_residences/bookings/excel/excelall.blade.php
+++ resources/views/sanitary_residences/bookings/excel/excelall.blade.php
@@ -42,7 +42,7 @@
         @foreach ($bookings as $booking)
         @if ($booking->status == 'Residencia Sanitaria' and $booking->patient->suspectCases->last()->status == 'Residencia Sanitaria')
         <tr>
-            <td nowrap>{{$booking->count()}}</td>
+            <td nowrap>{{$loop->count - $loop->remaining}}</td>
             <td nowrap>Iquique</td>
             <td nowrap>Iquique</td>
             <td nowrap>{{$booking->room->residence->name}}</td>
