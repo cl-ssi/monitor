547e9ae491e313bcaa198ecaef61d10103afea97
AquaroTorres <alvarotorres@gmail.com>
2020-04-30 18:06:14 -0400

    Patient list enchulamiento

--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -21,39 +21,43 @@
 
 
 <div class="table-responsive">
-<table class="table table-sm">
+<table class="table table-sm small">
     <thead>
         <tr>
             <th>Run o (ID)</th>
             <th>Nombre</th>
             <th>Genero</th>
             <th>Fecha Nac.</th>
-            <th>Dirección/Comuna</th>
-            <th>Teléfono/Email</th>
-            @can('Patient: edit')
-            <th></th>
-            @endcan
+            <th>Comuna</th>
+            <th>Dirección</th>
+            <th>Teléfono</th>
+            <th>Email</th>
         </tr>
     </thead>
     <tbody id="tablePatients">
         @foreach($patients as $patient)
         <tr class="{{ ($patient->suspectCases->where('pscr_sars_cov_2','positive')->first())?'alert-danger':'' }}">
             <td class="text-center">{{ $patient->identifier }}</td>
-            <td>{{ $patient->fullName }}</td>
+            <td>
+                @canany(['Patient: edit','Demographic: edit'])
+                    <a href="{{ route('patients.edit',$patient) }}">
+                @endcan
+                {{ $patient->fullName }}
+                @canany(['Patient: edit','Demographic: edit'])
+                    </a>
+                @endcan
+            </td>
             <td>{{ $patient->genderEsp }}</td>
             <td nowrap>{{ ($patient->birthday)?$patient->birthday->format('d-m-Y'):'' }}</td>
+            <td nowrap>{{ ($patient->demographic)?$patient->demographic->commune:'' }}</td>
             <td class="small">
                 {{ ($patient->demographic)?$patient->demographic->address:'' }}
-                {{ ($patient->demographic)?$patient->demographic->number:'' }}<br>
-                {{ ($patient->demographic)?$patient->demographic->commune:'' }}
+                {{ ($patient->demographic)?$patient->demographic->number:'' }}
             </td>
             <td class="small">
-                {{ ($patient->demographic)?$patient->demographic->telephone:'' }}<br>
-                {{ ($patient->demographic)?$patient->demographic->email:'' }}
+                {{ ($patient->demographic)?$patient->demographic->telephone:'' }}
             </td>
-            @canany(['Patient: edit','Demographic: edit'])
-            <td> <a href="{{ route('patients.edit',$patient) }}">Editar</a> </td>
-            @endcan
+            <td>{{ ($patient->demographic)?$patient->demographic->email:'' }}</td>
         </tr>
         @endforeach
     </tbody>
