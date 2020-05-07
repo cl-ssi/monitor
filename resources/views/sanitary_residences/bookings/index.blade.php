225ef9eaae8a827aca90c00b6cc6b579af55bd7e
Esteban <tebiccr@gmail.com>
2020-05-07 16:52:56 -0400

    como antes

--- resources/views/sanitary_residences/bookings/index.blade.php
+++ resources/views/sanitary_residences/bookings/index.blade.php
@@ -75,7 +75,7 @@
     </thead>
     <tbody class="small">        
         @foreach($bookings as $booking)
-        @if($booking->real_to and $booking->room->residence->id==$recidence->id)
+        @if($booking->real_to)
         <tr>
             <td>{{ $booking->patient->fullName }}</td>
             <td>{{ $booking->status }}</td>
