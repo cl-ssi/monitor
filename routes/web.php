7c6af2c9b106bca786b80cda9158f3e201d8e418
AquaroTorres <alvarotorres@gmail.com>
2020-05-07 15:56:15 -0400

    Reporte de positivos y reorden del menu

--- routes/web.php
+++ routes/web.php
@@ -89,6 +89,9 @@ Route::prefix('lab')->name('lab.')->group(function () {
         Route::get('/{suspect_case}/edit','SuspectCaseController@edit')->name('edit')->middleware('auth','can:SuspectCase: edit');
         Route::put('/{suspect_case}','SuspectCaseController@update')->name('update')->middleware('auth','can:SuspectCase: edit');
         Route::delete('/{suspect_case}','SuspectCaseController@destroy')->name('destroy')->middleware('auth','can:SuspectCase: delete');
+        Route::prefix('reports')->name('reports.')->group(function () {
+            Route::get('/positives','SuspectCaseReportController@positives')->name('positives')->middleware('auth','can:Report');
+        });
         Route::prefix('report')->name('report.')->group(function () {
             Route::get('/','SuspectCaseController@report')->name('index')->middleware('auth','can:Report');
             Route::get('historical_report','SuspectCaseController@historical_report')->name('historical_report')->middleware('auth','can:Report');
