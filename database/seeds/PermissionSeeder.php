b8536e94da773d8175fa28b830423449edd5e7b0
AquaroTorres <alvarotorres@gmail.com>
2020-05-16 23:00:14 -0400

    SuspectCase: edit tecnologo

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -32,6 +32,7 @@ class PermissionSeeder extends Seeder
         $permission = Permission::create(['name' => 'SuspectCase: tecnologo']);
         $permission = Permission::create(['name' => 'SuspectCase: seremi']);
         $permission = Permission::create(['name' => 'SuspectCase: reception']);
+        $permission = Permission::create(['name' => 'SuspectCase: edit tecnologo']);
 
         $permission = Permission::create(['name' => 'Report: positives']);
         $permission = Permission::create(['name' => 'Report: other']);
@@ -40,7 +41,7 @@ class PermissionSeeder extends Seeder
         $permission = Permission::create(['name' => 'Epp: list']);
 
         $permission = Permission::create(['name' => 'SanitaryResidence: user']);
-        $permission = Permission::create(['name' => 'SanitaryResidence: admin']);        
+        $permission = Permission::create(['name' => 'SanitaryResidence: admin']);
 
         $permission = Permission::create(['name' => 'File_report: viewer']);
 
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,14 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">Iniciar</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Correo electrónico</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Clave</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
