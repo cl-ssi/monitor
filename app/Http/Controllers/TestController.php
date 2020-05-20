25f73228946e7a9dbb8957f8bdb7cb4dfe53d225
AquaroTorres <alvarotorres@gmail.com>
2020-05-20 13:56:41 -0400

    WS fonasa beautifier wsdl

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,39 +15,32 @@ class TestController extends Controller
     {
         $rut = 15287582;
         $dv  = 7;
-	$wsdl = asset('ws/fonasa/CertificadorPrevisionalSoap.wsdl');
+        $wsdl = asset('ws/fonasa/CertificadorPrevisionalSoap.wsdl');
 
         $client = new \SoapClient($wsdl,array('trace'=>TRUE));
-	//$client->SomeFunction(
-        $parametros = array("query" =>
-                                array("queryTO" => array("tipoEmisor"  => 3,"tipoUsuario" => 2),
-                                "entidad" => env('FONASA_ENTIDAD'),
-                                "claveEntidad" => env('FONASA_CLAVE'),
-                                "rutBeneficiario" => $rut,
-                                "dgvBeneficiario" => $dv,
-                                "canal" =>3)
-                            );
-	$functions = $client->__getFunctions();
-	echo '<pre>';
-	var_dump($functions);
-	
+        $parameters = array(
+            "query" => array(
+                "queryTO" => array(
+                    "tipoEmisor"  => 3,
+                    "tipoUsuario" => 2
+                ),
+                "entidad" => env('FONASA_ENTIDAD'),
+                "claveEntidad" => env('FONASA_CLAVE'),
+                "rutBeneficiario" => $rut,
+                "dgvBeneficiario" => $dv,
+                "canal" =>3
+            )
+        );
 
-        $client->soap_defencoding = 'UTF-8';
-	$result = $client->getCertificadoPrevisional($parametros);
-	//$result = $client->__soapCall("getCertificadoPrevisional", $parametros);
-       	//"http://ws.fonasa.cl:8080/Certificados/Previsional");
-        var_dump($result); //cuando trae false no conecta :c, del cotrario si c:
+        //$client->soap_defencoding = 'UTF-8';
+        $result = $client->getCertificadoPrevisional($parameters);
+
+        echo '<pre>';
+        print_r($parameters);
+
+        //var_dump($result); //cuando trae false no conecta :c, del cotrario si c:
         // if ($result === false){
         //     $result = array("status" => "noContetar");
         // }
-
-        die('pum');
-        // {{ asset('ws/fonasa/CertificadorPrevisionalSoap.wsdl') }}
-        /*
-		$vlsDig='';
-		$vlsCanal=3;
-		$vlsUsuario=2;
-		$vlsTipo=3;
-        */
     }
 }
