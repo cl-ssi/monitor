00d60898861d78d36f23dba0002496b9c17ba539
AquaroTorres <alvarotorres@gmail.com>
2020-05-24 01:03:33 -0400

    Nuevo home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,22 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    Bienvenido al sistema de monitoreo del Servicio de Salud
+            <div class="card bg-light">
+                <div class="card-header text-center">
+                    <h4>Bienvenido al sistema de monitoreo del Servicio de Salud</h4>
                 </div>
 
-                <div class="card-body">
+                <div class="card-body text-center" style="background-color: #DADEDF;">
+
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
+                    <img src="{{ asset('images/esmeralda.png') }}" width="300" alt="Foto de la esmeralda">
 
+                </div>
+                <div class="card-footer text-muted text-center">
                     Sistema desarrollado por el área de Informática del
                     Servicio de Salud de Iquique
                     <a href="mailto:sistemas.ssi@redsalud.gob.cl">
