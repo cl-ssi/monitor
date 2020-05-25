0539e6b4aecafd80b47aeb866f2b0567e6a9d63b
Esteban <tebiccr@gmail.com>
2020-05-25 16:03:46 -0400

    con mantenedor de lab

---
+++ resources/views/lab/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('title', 'Listado de Laboratorios')
+
+@section('content')
+
+
+<h3 class="mb-3">Listado de Laboratorios</h3>
+
+<a class="btn btn-primary mb-3" href="{{ route('lab.create') }}">Crear nuevo laboratorio</a>
+
+<table class="table table-sm table-bordered">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Externo</th>            
+            <th>Editar</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($laboratories as $laboratory)
+        <tr>
+            <td>{{ $laboratory->name }}</td>
+            <td>
+                @if ($laboratory->external == 1 )
+                Sí                
+                @else
+                No
+                @endif
+            </td>
+            <td>
+            <a href="{{ route('lab.edit', $laboratory) }}" class="btn btn-secondary float-left"><i class="fas fa-edit"></i></a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+
+@endsection
+
+@section('custom_js')
+
+@endsection
