317dd629d4a82a4fbf050bb615310bf2f268d3a2
Esteban <tebiccr@gmail.com>
2020-05-26 19:00:45 -0400

    excel suspectCase

--- app/Exports/SuspectCasesExport.php
+++ app/Exports/SuspectCasesExport.php
@@ -67,7 +67,7 @@ class SuspectCasesExport implements FromCollection, WithHeadings, WithMapping, W
         return [
             $suspectCase->id,
             Date::dateTimeToExcel($suspectCase->sample_at),
-            $suspectCase->origin,
+            $suspectCase->establishment->alias,
             ($suspectCase->patient)?$suspectCase->patient->fullName:'',
             ($suspectCase->patient)?$suspectCase->patient->Identifier:'',
             $suspectCase->age,
