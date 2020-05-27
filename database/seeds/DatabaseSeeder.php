e64f76ed2f593037648ba9e4e72d131b735b58ce
AquaroTorres <alvarotorres@gmail.com>
2020-05-26 21:58:38 -0400

    country

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
         $this->call(RegionSeeder::class);
         $this->call(CommuneSeeder::class);
         $this->call(EstablishmentSeeder::class);
+        $this->call(CountrySeeder::class);
         $this->call(LaboratorySeeder::class);
         $this->call(PermissionSeeder::class);
         $this->call(UserSeeder::class);
-        $this->call(CountrySeeder::class);
         $this->call(ResidenceSeeder::class);
         $this->call(RoomSeeder::class);
         $this->call(VentilatorSeeder::class);
