879a40d68ba9c1ac3398d37ca21175f22eecadb6
Esteban <tebiccr@gmail.com>
2020-05-29 12:23:51 -0400

    required en html de paciente

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -46,14 +46,14 @@
 
     <div class="form-row">
         <fieldset class="form-group col-12 col-md-4">
-            <label for="for_name">Nombres</label>
+            <label for="for_name">Nombres*</label>
             <input type="text" class="form-control" id="for_name" name="name"
                 style="text-transform: uppercase;"
                 required>
         </fieldset>
 
         <fieldset class="form-group col-12 col-md-4">
-            <label for="for_fathers_family">Apellido Paterno</label>
+            <label for="for_fathers_family">Apellido Paterno*</label>
             <input type="text" class="form-control" id="for_fathers_family"
                 name="fathers_family" style="text-transform: uppercase;"
                 required>
