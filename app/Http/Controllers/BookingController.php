039bc48906292ef350f3122fd8d45bd3e1fe3e33
Esteban <tebiccr@gmail.com>
2020-05-29 19:48:17 -0400

    se ordena el booking por piso y luego n°

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -20,7 +20,7 @@ class BookingController extends Controller
      */
     public function index(Residence $residence)
     {
-        $rooms = $rooms = Room::where('residence_id',$residence->id)->orderBy('floor')->get();
+        $rooms = $rooms = Room::where('residence_id',$residence->id)->orderBy('floor')->orderBy('number')->get();
         $bookings = Booking::All();
         return view('sanitary_residences.bookings.index', compact('residence','bookings', 'rooms'));
     }
