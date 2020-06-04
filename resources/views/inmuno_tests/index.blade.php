ec0f1061a5fae8fe06dc43482734af9052e58ee7
mirandal.jorge <mirandal.jorge@gmail.com>
2020-06-04 16:43:14 -0400

    Inmuno Test: Fix Search

--- resources/views/inmuno_tests/index.blade.php
+++ resources/views/inmuno_tests/index.blade.php
@@ -13,6 +13,22 @@
 </div>
 @endcan
 
+<div align="right">
+<form method="get" action="{{ route('lab.inmuno_tests.index') }}">
+    <div class="input-group mb-3 col-sm-8">
+    <div class="input-group-prepend">
+        <span class="input-group-text">Búsqueda</span>
+    </div>
+
+    <input class="form-control" type="text" name="search" value="{{$request->search}}" placeholder="RUN (sin dígito verificador) o OTRA IDENTIFICACION / NOMBRE">
+    <div class="input-group-append">
+        <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i> Buscar</button>
+    </div>
+  </div>
+
+</form>
+</div>
+
 <table class="table table-sm small table-bordered">
     <thead>
         <tr class="text-center">
