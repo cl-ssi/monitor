70a552ed7ebf9d7b76b682b8f14c3e20f78cbf6a
AquaroTorres <alvarotorres@gmail.com>
2020-06-13 23:48:42 -0400

    Modelos y migraciones de seguimiento

---
+++ app/Tracing/EventType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tracing;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class EventType extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    public function events() {
+        return $this->hasMany('App\Tracing\Event');
+    }
+}
---
+++ database/migrations/2020_06_13_143304_create_tracing_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTracingEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracing_events', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('event_at');
+            $table->foreignId('event_type_id');
+            $table->text('details')->nullable();
+            $table->foreignId('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('event_type_id')->references('id')->on('tracing_event_types');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tracing_events');
+    }
+}
