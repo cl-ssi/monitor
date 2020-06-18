eb94e3b3f1576b9b73307980795ca5500535b214
AquaroTorres <alvarotorres@gmail.com>
2020-06-17 23:43:50 -0400

    Paginate old tracing

--- resources/views/lab/suspect_cases/reports/case_tracing.blade.php
+++ resources/views/lab/suspect_cases/reports/case_tracing.blade.php
@@ -6,20 +6,26 @@
 
 <h3 class="">Seguimiento de Casos Positivos</h3>
 
-</main><main class="">
-
-    <h4>Casos sin domicilio</h4>
+    <h4>Casos sin comuna</h4>
     @include('lab.suspect_cases.reports.partials.table_case_tracing',['patients' => $patientsNoDemographic,
                                                                         'max_cases' => $max_cases_no_demographic,
                                                                         'max_cases_inmuno' => $max_cases_inmuno_no_demographic])
 
     <br/>
 
-    <div class="form-inline">
-        <h4 style="padding-right:10px">Casos con domicilio</h4>
-        <a class="btn btn-outline-success btn-sm mb-3" id="downloadLink" style="line" href="{{ route('lab.suspect_cases.reports.case_tracing.export') }}" >Descargar en excel</a>
-    </div>
+    <a class="btn btn-outline-success btn-sm mb-3" href="{{ route('lab.suspect_cases.reports.case_tracing.export') }}">
+        Descargar en excel
+    </a>
 
     @include('lab.suspect_cases.reports.partials.table_case_tracing')
 
+    {{ $patients->links() }}
+@endsection
+
+@section('custom_js')
+<script type="text/javascript">
+$(document).ready(function(){
+    $("main").removeClass("container");
+});
+</script>
 @endsection
