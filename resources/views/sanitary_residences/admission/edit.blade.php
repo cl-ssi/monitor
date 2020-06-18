b9a586811d5a90db94822f88b91c6e985222a977
Esteban <tebiccr@gmail.com>
2020-06-18 04:50:41 -0400

    modulo encuesta con cambios de Luis Peña

--- resources/views/sanitary_residences/admission/edit.blade.php
+++ resources/views/sanitary_residences/admission/edit.blade.php
@@ -6,14 +6,14 @@
 
 
 
-<h3 class="mb-3">Pauta de Evaluación Residencia Sanitaria</h3>
+<h3 class="mb-3">Editar Pauta de Evaluación Residencia Sanitaria</h3>
 
 <hr>
 <form method="POST" class="form-horizontal" action=" {{ route('sanitary_residences.admission.update', $admission) }} ">
     @csrf
     @method('PUT')
 
-    
+    <h5 class="mb-6">Identificación de Paciente</h5>
 
     <div class="form-row">
         <fieldset class="form-group col-8 col-md-8">
@@ -47,9 +47,33 @@
 
     <div class="form-row">
 
+        <fieldset class="form-group col-2 col-md-2">
+            <label for="for_name">Telefono</label>
+            <input type="text" class="form-control" name="name" id="for_name" required placeholder="" autocomplete="off" value="{{ ($admission->patient->demographic)?$admission->patient->demographic->telephone:'' }}" readonly>
+        </fieldset>        
+
+        <fieldset class="form-group col-2 col-md-2">
+            <label for="for_name">Edad</label>
+            <input type="text" class="form-control" name="name" id="for_name" required placeholder="" autocomplete="off" value="{{ $admission->patient->age }}" readonly>
+        </fieldset>
+
+        <fieldset class="form-group col-4 col-md-4">
+            <label for="for_name">Fecha de Confirmación de COVID 19- Positivo</label>
+            <input type="text" class="form-control" name="name" id="for_name" required placeholder="" autocomplete="off" value="{{ ($admission->patient->suspectCases->where('pscr_sars_cov_2', 'positive')->last())? $admission->patient->suspectCases->where('pscr_sars_cov_2', 'positive')->last()->pscr_sars_cov_2_at->format('d-m-Y H:i'):''  }}" readonly>
+        </fieldset>
+
+        <fieldset class="form-group col-3 col-md-3">
+            <label for="for_address">Centro de Salud de Origen</label>
+            <input type="text" class="form-control" name="address" id="for_address" required placeholder="" autocomplete="off" value="{{ ($admission->patient->suspectCases->where('pscr_sars_cov_2', 'positive')->last())? $admission->patient->suspectCases->where('pscr_sars_cov_2', 'positive')->last()->origin:''  }}" readonly>
+        </fieldset>
+        
+    </div>
+
+    <div class="form-row">
+
         <fieldset class="form-group col-4 col-md-4">
             <label for="for_prevision">Previsión</label>
-            <select name="prevision" id="for_prevision" class="form-control" required disabled>
+            <select name="prevision" id="for_prevision" class="form-control" required>
                 <option value="">Elegir Previsión</option>
                 <option value="Sin Previsión" {{ ($admission->prevision == 'Sin Previsión')?'selected':'' }}>Sin Previsión</option>
                 <option value="Fonasa A" {{ ($admission->prevision == 'Fonasa A')?'selected':'' }}>Fonasa A</option>
@@ -61,42 +85,13 @@
                 <option value="OTRO" {{ ($admission->prevision == 'OTRO')?'selected':'' }}>OTRO</option>
             </select>
         </fieldset>
-
-        <fieldset class="form-group col-2 col-md-2">
-            <label for="for_name">Edad</label>
-            <input type="text" class="form-control" name="name" id="for_name" required placeholder="" autocomplete="off" value="{{ $admission->patient->age }}" readonly>
-        </fieldset>
-
-        <fieldset class="form-group col-4 col-md-4">
-            <label for="for_address">Fecha y Hora de Notificación de Resultado</label>
-            <input type="text" class="form-control" name="address" id="for_address" required placeholder="" autocomplete="off" value="{{  ($admission->patient->suspectCases->where('pscr_sars_cov_2', 'positive')->last())? $admission->patient->suspectCases->where('pscr_sars_cov_2', 'positive')->last()->pscr_sars_cov_2_at->format('d-m-Y H:i'):''   }}" readonly>
-        </fieldset>
-
-
-        <fieldset class="form-group col-2 col-md-2">
-            <label for="for_name">Telefono</label>
-            <input type="text" class="form-control" name="name" id="for_name" required placeholder="" autocomplete="off" value="{{ ($admission->patient->demographic)?$admission->patient->demographic->telephone:'' }}" readonly>
-        </fieldset>
-    </div>
-
-    <div class="form-row">
         
         <fieldset class="form-group col-3 col-md-3">
             <label for="for_address">Telefono(s) Contacto Emergencia</label>
-            <input type="text" class="form-control" name="contactnumber" id="for_contactnumber" placeholder="" autocomplete="off" value="{{$admission->contactnumber}}" readonly>
-        </fieldset>
+            <input type="text" class="form-control" name="contactnumber" id="for_contactnumber" placeholder="" autocomplete="off" value="{{$admission->contactnumber}}">
+        </fieldset>        
 
-
-
-        <fieldset class="form-group col-4 col-md-4">
-            <label for="for_name">Fecha de Confirmación de COVID 19- Positivo</label>
-            <input type="text" class="form-control" name="name" id="for_name" required placeholder="" autocomplete="off" value="{{ ($admission->patient->suspectCases->where('pscr_sars_cov_2', 'positive')->last())? $admission->patient->suspectCases->where('pscr_sars_cov_2', 'positive')->last()->pscr_sars_cov_2_at->format('d-m-Y H:i'):''  }}" readonly>
-        </fieldset>
-
-        <fieldset class="form-group col-3 col-md-3">
-            <label for="for_address">Centro de Salud de Origen</label>
-            <input type="text" class="form-control" name="address" id="for_address" required placeholder="" autocomplete="off" value="{{ ($admission->patient->suspectCases->where('pscr_sars_cov_2', 'positive')->last())? $admission->patient->suspectCases->where('pscr_sars_cov_2', 'positive')->last()->origin:''  }}" readonly>
-        </fieldset>
+        
 
     </div>
 
@@ -104,12 +99,12 @@
     <div class="form-row">
         <fieldset class="form-group col-12 col-md-7">
             <label for="for_indications">Antecedentes Morbidos</label>
-            <textarea class="form-control" id="for_morbid_history" rows="6" name="morbid_history" readonly>{{$admission->morbid_history}}</textarea>
+            <textarea class="form-control" id="for_morbid_history" rows="6" name="morbid_history" >{{$admission->morbid_history}}</textarea>
         </fieldset>
 
         <fieldset class="form-group col-12 col-md-5">
             <label for="for_observations">Observaciones</label>
-            <textarea type="textarea" class="form-control" rows="6" name="observations" id="for_observations" readonly>{{$admission->observations}}</textarea>
+            <textarea type="textarea" class="form-control" rows="6" name="observations" id="for_observations" >{{$admission->observations}}</textarea>
         </fieldset>
     </div>
 
@@ -118,7 +113,7 @@
 
     <div class="form-row">
         <fieldset class="form-group col-4 col-md-4">
-            <label for="for_name">¿Con cuantas personas habita?</label>
+            <label for="for_name">¿Cuantas personas viven en su domicilio?</label>
             <input type="number" class="form-control" name="people" id="for_people" required placeholder="" autocomplete="off" value="{{$admission->people}}" readonly>
         </fieldset>
         <fieldset class="form-group col-4 col-md-4">
@@ -130,6 +125,22 @@
             <input type="number" class="form-control" name="bathrooms" id="for_bathrooms" required placeholder="" autocomplete="off" value="{{$admission->bathrooms}}" readonly>
         </fieldset> 
     </div>
+
+    <div class="form-check">
+        <h5 class="mb-6">¿Tiene Agua en su Domicilio?</h5>
+        <input class="form-check-input" type="radio" name="water" id="exampleRadios1" value="1" required>
+        <label class="form-check-label" for="exampleRadios1">
+            SÍ
+        </label>
+    </div>
+
+    <div class="form-check">
+        <input class="form-check-input" type="radio" name="water" id="exampleRadios2" value="0">
+        <label class="form-check-label" for="exampleRadios2">
+            NO
+        </label>
+    </div>
+    <br>
     
     <div class="form-check">
         <h5 class="mb-6">¿ES POSIBLE AISLAR AL PACIENTE? (Seguir con la encuesta si el paciente no puede aislarse correctamente)</h5>
@@ -167,37 +178,22 @@
             <td class="text-center"><input name="basicactivities" id="for_basicactivities" type="radio" value="0" {{ ($admission->basicactivities=='0')?'checked':'disabled' }}></td>
         </tr>
         <tr>
-            <td>¿Consume alcohol habitualmente? ¿consume drogas?</td>
+            <td>¿Actualmente se encuentra o se ha encontrado en tratamiento por alguna adicción (como alcohol, drogas o tabaco)?</td>
             <td class="text-center"><input name="drugs" id="for_drugs" type="radio" value="1" {{ ($admission->drugs=='1')?'checked':'disabled' }}></td>
             <td class="text-center"><input name="drugs" id="for_drugs" type="radio" value="0" {{ ($admission->drugs=='0')?'checked':'disabled' }}></td>
         </tr>
         <tr>
-            <td>¿Tiene enfermedades crónicas no compensadas o que no estén compensadas?</td>
+            <td>¿Tiene enfermedades crónicas no compensadas (ej: hipertensión, diabetes)?</td>
             <td class="text-center"><input name="chronic" id="for_chronic" type="radio" value="1" {{ ($admission->chronic=='1')?'checked':'disabled' }}></td>
             <td class="text-center"><input name="chronic" id="for_chronic" type="radio" value="0" {{ ($admission->chronic=='0')?'checked':'disabled' }}></td>
         </tr>
         <tr>
-            <td>¿Tiene algún otor problema de salud en este momento?</td>
+            <td>¿Tiene algún otro problema de salud en este momento?</td>
             <td class="text-center"><input name="healthnow" id="for_healthnow" type="radio" value="1" {{ ($admission->healthnow=='1')?'checked':'disabled' }}></td>
             <td class="text-center"><input name="healthnow" id="for_healthnow" type="radio" value="0" {{ ($admission->healthnow=='0')?'checked':'disabled' }}></td>
-        </tr>
+        </tr>                
         <tr>
-            <td>¿Posee agua en su domicilio?</td>
-            <td class="text-center"><input name="water" type="radio" value="1" {{ ($admission->water=='1')?'checked':'disabled' }}></td>
-            <td class="text-center"><input name="water" type="radio" value="0" {{ ($admission->water=='0')?'checked':'disabled' }}></td>
-        </tr>
-        <tr>
-            <td>¿Usted requiere salir para trabajar pese a estar en cuarentena?</td>
-            <td class="text-center"><input name="work" type="radio" value="1" {{ ($admission->work=='1')?'checked':'disabled' }}></td>
-            <td class="text-center"><input name="work" type="radio" value="0" {{ ($admission->work=='0')?'checked':'disabled' }}></td>
-        </tr>
-        <tr>
-            <td>¿Tiene red de apoyo para que le traigan alimento?</td>
-            <td class="text-center"><input name="food" type="radio" value="1" {{ ($admission->food=='1')?'checked':'disabled' }}></td>
-            <td class="text-center"><input name="food" type="radio" value="0" {{ ($admission->food=='0')?'checked':'disabled' }}></td>
-        </tr>
-        <tr>
-            <td>¿Tiene personas de riesgos médico en su hogar?</td>
+            <td>¿Convive usted con personas portadoras de enfermedades crónicas (ej: Cancer, Hierpertención, Diabetes, Lupus, etc)?</td>
             <td class="text-center"><input name="risk" type="radio" value="1" {{ ($admission->risk=='1')?'checked':'disabled' }}></td>
             <td class="text-center"><input name="risk" type="radio" value="0" {{ ($admission->risk=='0')?'checked':'disabled' }}></td>
         </tr>
@@ -232,16 +228,26 @@
             NO
         </label>
     </div>
-
-
-
-
-
-    <button type="submit" class="btn btn-primary">Guardar</button>
+    
+    
+    @canany(['SanitaryResidence: survey','Developer'])
+    <button type="submit" class="btn btn-primary">Actualizar Datos</button>
+    @endcan
 
     <a class="btn btn-outline-secondary" href="{{ URL::previous() }}">Cancelar</a>
 
-</form>
+    </form>
+    <br>
+    <br>
+
+    @can('SanitaryResidence: admission')
+    <div class="text-center">
+        <a class="btn btn-success text-center" href="{{ route('sanitary_residences.admission.changestatus', $admission) }}">VISTO BUENO INGRESO</a>
+        <a class="btn btn-danger text-center" href="{{ route('sanitary_residences.admission.changestatus', $admission) }}">RECHAZO INGRESO</a>        
+    </div>
+    @endcan
+
+
 
 
 @endsection
