4488a9dbca958c18ddecedaac6a87132cffae4c6
GermanZun <german.zuniga@gmail.com>
2020-06-19 14:37:57 -0400

    Creación de usuarios validado no rut repetido

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,7 +8,15 @@
 
 <h3 class="mb-3">Nuevo Usuario</h3>
 
-
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 
 <form method="POST" class="form-horizontal" action="{{ route('users.store') }}">
     @csrf
