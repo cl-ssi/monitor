e244737320599e690ba3e558800bb17a708faa63
AquaroTorres <alvarotorres@gmail.com>
2020-06-22 17:22:43 -0400

    Fallecidos

--- app/Http/Controllers/SuspectCaseReportController.php
+++ app/Http/Controllers/SuspectCaseReportController.php
@@ -636,6 +636,9 @@ class SuspectCaseReportController extends Controller
         return view('lab.suspect_cases.reports.positivesByDateRange', compact('suspectCases', 'from', 'to'));
     }
 
+    /*****************************************************/
+    /*            REPORTE HOSPITALIZADOS                 */
+    /*****************************************************/
     public function hospitalized() {
         $patients = Patient::whereIn('status',[
             'Hospitalizado Básico',
@@ -648,6 +651,14 @@ class SuspectCaseReportController extends Controller
         ->get();
 
         return view('lab.suspect_cases.reports.hospitalized', compact('patients'));
+    }
+
+    /*****************************************************/
+    /*            REPORTE FALLECIDOS                     */
+    /*****************************************************/
+    public function deceased() {
+        $patients = Patient::where('status','Fallecido')->orderBy('deceased_at')->get();
+        return view('lab.suspect_cases.reports.deceased', compact('patients'));
     }
 
 
