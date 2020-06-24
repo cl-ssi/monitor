812cc20c919ac18249f4cf03dd1bdb70f90e9290
Esteban <tebiccr@gmail.com>
2020-06-23 22:16:09 -0400

    fix aprobados SEREMI

--- resources/views/sanitary_residences/nav.blade.php
+++ resources/views/sanitary_residences/nav.blade.php
@@ -28,54 +28,6 @@
 
     @endcan
 
-    <!-- <li class="nav-item">
-        <a class="nav-link" href="{{ route('sanitary_residences.admission.index') }}">        
-            <i class="fas fa-clipboard-check"></i> Aprobados
-        </a>
-    </li> -->
-
     
-    
-    
-
-
-    
-    <!-- <li class="nav-item">
-        <a class="nav-link" href="{{ route('sanitary_residences.residences.index') }}">
-            <i class="fas fa-hotel"></i> Residencias
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('sanitary_residences.rooms.index') }}">
-            <i class="fas fa-home"></i> Habitaciones
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('sanitary_residences.users') }}">
-            <i class="fas fa-users-cog"></i> Usuarios
-        </a>
-    </li> -->
-
-
-    <!-- <li class="nav-item">
-        <a class="nav-link"  href="{{ route('sanitary_residences.residences.statusReport') }}">
-            <i class="fas fa-file-excel"></i> Consolidado Booking
-        </a>
-    </li> -->
-
-    <!-- <li class="nav-item">
-        <a class="nav-link" href="{{ route('sanitary_residences.bookings.bookingByDate') }}">
-            <i class="fas fa-file-excel"></i> Booking Realizados por Fechas
-        </a>
-    </li>
-    
-
-     <li class="nav-item">
-        <a class="nav-link"  href="{{ route('sanitary_residences.bookings.excelvitalsign')  }}">
-            <i class="fas fa-file-excel"></i> Reporte Signos Vitales
-        </a>
-    </li> -->
 
 </ul>
