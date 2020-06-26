c02bfb170057cff374e4f2edc354fa8c941abb42
Esteban <tebiccr@gmail.com>
2020-06-26 00:58:37 -0400

    probando descarga PDF clave única

--- resources/views/lab/result.blade.php
+++ resources/views/lab/result.blade.php
@@ -14,7 +14,7 @@
             <th>Fecha de toma de muestra</th>
             <th>Resultado COVID19</th>
             <th>Fecha del resultado</th>            
-            <th></th>
+            <th>Descargar Examen</th>
         </tr>
     </thead>
     <tbody>
@@ -26,6 +26,9 @@
             <td>{{ ($case->pscr_sars_cov_2_at) ? $case->pscr_sars_cov_2_at : '' }}</td>
             <td>
                 @if($case->pscr_sars_cov_2 != 'positive')
+                <a href="{{ route('lab.print', $case) }}"
+                    target="_blank"><i class="fas fa-paperclip"></i>&nbsp
+                </a>
                 <!-- <a href="{{ route('lab.suspect_cases.download', $case->files->first()->id) }}"
                     target="_blank" data-toggle="tooltip" data-placement="top"
                     data-original-title="resultado_{{$case->patient->run}}.pdf">
