79af8b6a7f268ee8f0b62179057f2e1894c47a3f
GermanZun <german.zuniga@gmail.com>
2020-06-30 15:08:11 -0400

    se agregan reportes en residencia, finalizados

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -110,7 +110,6 @@
                                 <a class="dropdown-item" href="{{ route('patients.tracings.communes') }}">Seguimiento de mis comunas</a>
                                 <a class="dropdown-item" href="{{ route('patients.tracings.establishments') }}">Seguimiento de mis establecimientos</a>
                                 <a class="dropdown-item" href="{{ route('lab.suspect_cases.reports.tracing_minsal') }}">Seguimiento SEREMI</a>
-                                <a class="dropdown-item" href="{{ route('patients.tracings.completed') }}">Seguimientos finalizados</a>
                                     @canany(['SocialTracing: seremi', 'SocialTracing: seremi'])
                                 <a class="dropdown-item" href="{{ route('patients.tracings.requests.social_index') }}">Seguimiento de solicitudes</a>
                                 @endcanany
