a3af1efc61273de230b50d8f2fd182a4b85f4b87
Esteban <tebiccr@gmail.com>
2020-07-04 12:20:18 -0400

    4 horas signos vitales

--- resources/views/sanitary_residences/home.blade.php
+++ resources/views/sanitary_residences/home.blade.php
@@ -10,7 +10,7 @@
 <h4 class="mb-3" style="text-align:center"> Se les recuerda lo Siguiente</h4>
 <ul style="list-style-type:disc; font-size:15px">
   <li>No se debe compartir el usuario y contraseña con otros compañeros.</li>
-  <li>Se deberá de digitar en el sistema con un máximo de 3 horas desde que se tomó el signo vital</li>
+  <li>Se deberá de digitar en el sistema con un máximo de 4 horas desde que se tomó el signo vital</li>
   <li>La asignación de la (o las) residencia sanitaria que deberá de cumplir sus labores lo realiza su jefatura, en caso que no le salga ninguna al momento de ingresar al módulo se tendrá que comunicar con ellos.</li>
   <li>Si comete un error de digitación (ej: dos veces digitados un paciente en el mismo cuarto) y necesita borrar un booking. Deberá de comunicarse con su jefatura ya que ellos tienen los privilegios para realizar esta acción.</li>
   <li>En caso de Traslado de paciente a otra Residencia Sanitaria, se deberá comunicar con su jefatura.</li>
