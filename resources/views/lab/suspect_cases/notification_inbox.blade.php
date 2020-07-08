074ebd16f319525b9fa2ab9f52fdb6b253d6a072
AquaroTorres <alvarotorres@gmail.com>
2020-07-07 23:41:11 -0400

    Notification Inbox

--- resources/views/lab/suspect_cases/notification_inbox.blade.php
+++ resources/views/lab/suspect_cases/notification_inbox.blade.php
@@ -1,52 +1,28 @@
 @extends('layouts.app')
 
-@section('title', 'Listado de casos')
+@section('title', 'Notificacion')
 
 @section('content')
 
 <h3 class="mb-3"><i class="fas fa-lungs-virus"></i>
-    Exámenes ingresados en las comunas del usuario.
+    Examenes por notificar, excluye positivos y pendientes
 </h3>
-
-{{-- <table class="table table-sm table-bordered">
-    <thead>
-        <tr class="text-center">
-            <th>Exámenes enviados a análisis</th>
-            <th>Exámenes positivos</th>
-            <th>Exámenes negativos</th>
-            <th>Exámenes pendientes</th>
-            <th>Exámenes rechazados</th>
-            <th>Exámenes indeterminados</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr class="text-center">
-            <td>{{ $suspectCasesTotal->count() }}</td>
-            <th class="text-danger">{{ $suspectCasesTotal->where('pscr_sars_cov_2','positive')->count() }}</th>
-            <td>{{ $suspectCasesTotal->where('pscr_sars_cov_2','negative')->count() }}</td>
-            <td>{{ $suspectCasesTotal->where('pscr_sars_cov_2','pending')->count() }}</td>
-            <td>{{ $suspectCasesTotal->where('pscr_sars_cov_2','rejected')->count() }}</td>
-            <td>{{ $suspectCasesTotal->where('pscr_sars_cov_2','undetermined')->count() }}</td>
-        </tr>
-    </tbody>
-</table> --}}
 
 
 <div class="table-responsive">
-{{-- <table class="table table-sm table-bordered" id="tabla_casos"> --}}
+
 <table class="table table-sm table-bordered table-responsive small text-uppercase" id="tabla_casos">
     <thead>
         <tr>
             <th nowrap>Id pac.</th>
-            {{-- <th>Origen</th> --}}
             <th>Nombre</th>
             <th>Comuna</th>
             <th>Dirección</th>
             <th>Teléfono</th>
-            <th style="width: 50%">Email</th>
+            <th>Email</th>
             <th>Establecimiento muestra</th>
-            <th>Fecha muestra</th>
-            <th>Fecha Resultado</th>
+            <th>F. muestra</th>
+            <th>F. Resultado</th>
             <th>Resultado</th>
             <th>Fecha Notificación</th>
             <th nowrap>Mecanismo de Notificación</th>
@@ -54,7 +30,7 @@
         </tr>
     </thead>
     <tbody id="tableCases">
-        @foreach($suspectCases as $case)
+        @foreach($suspectCases->reverse() as $case)
         <tr class="row_{{$case->covid19}} {{ ($case->pscr_sars_cov_2 == 'positive')?'table-danger':''}}">
             <td class="text-center">{{ $case->patient->id }}</td>
             <td>
@@ -70,18 +46,11 @@
             <td>{{ $case->patient->demographic->getFullAddressAttribute() }}</td>
             <td>{{ $case->patient->demographic->getFullTelephonesAttribute() }}</td>
             <td class="small">{{ $case->patient->demographic->email }}</td>
-            <td>{{ $case->establishment->name }}</td>
+            <td>{{ $case->establishment->alias }}</td>
             <td nowrap class="small">{{ (isset($case->sample_at))? $case->sample_at->format('Y-m-d'):'' }}</td>
             <td nowrap class="small">{{ (isset($case->pscr_sars_cov_2_at))? $case->pscr_sars_cov_2_at->format('Y-m-d'):'' }}</td>
-            {{--
-            <td class="text-center" nowrap>
-                @if($case->patient)
-                {{ $case->patient->identifier }}
-                @endif
-            </td> --}}
-            {{-- <td>{{ $case->age }}</td> --}}
-            {{-- <td>{{ strtoupper($case->gender[0]) }}</td> --}}
-            <td>
+
+            <td nowrap>
                 @if($case->laboratory)
                     {{ $case->covid19 }}
                     @if($case->files->first())
@@ -99,16 +68,16 @@
                     No recepcionado
                 @endif
             </td>
-            <form method="POST" action="{{ route('lab.updateNotification', $case) }}" class="d-inline" enctype="multipart/form-data">
+            <form method="POST" action="{{ route('lab.updateNotification', $case) }}" class="d-inline">
                 @csrf
     			@method('POST')
                 @if($case->notification_at)
-                    <td>{{ $case->notification_at }}</td>
+                    <td>{{ ($case->notification_at)? $case->notification_at->format('Y-m-d') : '' }}</td>
                     <td>{{ $case->notification_mechanism }}</td>
                 @else
-                    <td><input type="date" class="form-control" name="notification_at"
+                    <td><input type="date" class="form-control form-control-sm" name="notification_at"
                         id="for_notification_at" value="{{ ($case->notification_at)?$case->notification_at->format('Y-m-d'):'' }}"></td>
-                    <td><select name="notification_mechanism" id="for_notification_mechanism" class="form-control">
+                    <td><select name="notification_mechanism" id="for_notification_mechanism" class="form-control form-control-sm">
                         <option></option>
                         <option value="Pendiente"
                             {{ ($case->notification_mechanism == 'Pendiente')?'selected':'' }}>
@@ -129,14 +98,10 @@
                 @endif
 
                 @if(!$case->notification_at)
-                    {{-- <td nowrap class="small"> <a href="{{ route('lab.updateNotification', $case) }}"><span><i class="far fa-edit"></i></span> Guardar</a></td> --}}
+
                     <td nowrap class="small">
-                        {{-- <input type="submit"  value="guardar"/> --}}
-                        <button type="submit" class="btn btn-outline-secondary btn-sm">
-                         {{-- onclick="return confirm('¿Está seguro de subir la información?');"> --}}
-							<span style="font-size: 1em; color: #C9C8C8;">
+                        <button type="submit" class="btn btn-outline-primary btn-sm">
 							  <i class="far fa-save"></i>
-							</span>
 						</button>
                     </td>
                 @else
@@ -149,51 +114,13 @@
 </table>
 </div>
 
-{{ $suspectCases->appends(request()->query())->links() }}
 
 @endsection
 
 @section('custom_js')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
 <script type="text/javascript">
 $(document).ready(function(){
-    // $("#inputSearch").on("keyup", function() {
-    //     var value = $(this).val().toLowerCase();
-    //     $("#tableCases tr").filter(function() {
-    //         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    //     });
-    // });
-    // //oculta segun checkbox
-    // $("#chk_positivos").change(function(){
-    //     $(".row_Positivo").toggle();
-    // });
-    // $("#chk_negativos").change(function(){
-    //     var self = this;
-    //     $(".row_Negativo").toggle();
-    // });
-    // $("#chk_pendientes").change(function(){
-    //     var self = this;
-    //     $(".row_Pendiente").toggle();
-    // });
-    // $("#chk_rechazados").change(function(){
-    //     var self = this;
-    //     $(".row_Rechazado").toggle();
-    // });
-    // $("#chk_indeterminados").change(function(){
-    //     var self = this;
-    //     $(".row_Indeterminado").toggle();
-    // });
+    $("main").removeClass("container");
 });
-
-function exportF(elem) {
-    var table = document.getElementById("tabla_casos");
-    var html = table.outerHTML;
-    var html_no_links = html.replace(/<a[^>]*>|<\/a>/g, "");//remove if u want links in your table
-    var url = 'data:application/vnd.ms-excel,' + escape(html_no_links); // Set your html table into url
-    elem.setAttribute("href", url);
-    elem.setAttribute("download", "casos_sospecha.xls"); // Choose the file name
-    return false;
-}
 </script>
 @endsection
