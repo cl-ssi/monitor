ffcd7c00fb800fa4bfd75b949cbe49b332da4bd7
oscarzavala <ozavala@gmail.com>
2020-07-09 11:23:01 -0400

    Editar Paciente versión movil

--- resources/views/patients/tracing/partials/show.blade.php
+++ resources/views/patients/tracing/partials/show.blade.php
@@ -10,17 +10,18 @@
                 @csrf
                 @method('PUT')
                 <h4>Notificación</h4>
+
+<!--**********************************-->
                 <div class="form-row">
-                    <fieldset class="form-group col-md-2">
+                    <fieldset class="form-group col-12 col-sm-4">
                         <label for="for_notification_at">Fecha de Notificación</label>
                         <input type="date" class="form-control" name="notification_at"
                                id="for_notification_at"
                                value="{{ ($patient->tracing->notification_at) ? $patient->tracing->notification_at->format('Y-m-d') : '' }}">
                     </fieldset>
 
-
-                    <fieldset class="form-group col-6 col-md-3">
-                        <label for="for_notification_mechanism">Mecanismo de Notificación</label>
+                    <fieldset class="form-group col-12 col-sm-4">
+                        <label for="for_notification_mechanism">Mecanismo de Notifi.</label>
                         <select name="notification_mechanism" id="for_notification_mechanism" class="form-control">
                             <option></option>
                             <option value="Pendiente"
@@ -46,18 +47,18 @@
                         </select>
                     </fieldset>
 
-                    <fieldset class="form-group col-md-2">
+                    <fieldset class="form-group col-12 col-sm-4">
                         <label for="for_discharged_at">Fecha alta médica</label>
                         <input type="date" class="form-control" name="discharged_at"
                                id="for_discharged_at" value="{{ $patient->tracing->discharged_at }}">
                     </fieldset>
 
                 </div>
-
+<!--**********************************-->
                 <hr>
                 <h4 class="mt-4">Ficha inicial de Seguimiento</h4>
                 <div class="form-row">
-                    <fieldset class="form-group col-6 col-md-1">
+                    <fieldset class="form-group col-5">
                         <label for="for_index">Indice</label>
                         <select name="index" id="for_index" class="form-control" required>
                             <option value=""></option>
@@ -67,16 +68,16 @@
                         </select>
                     </fieldset>
 
-                    <fieldset class="form-group col-md-3">
+                    <fieldset class="form-group col-7">
                         <label for="for_next_control_at">Próximo seguimiento *</label>
                         <input type="datetime-local" class="form-control" name="next_control_at"
                                id="for_next_control_at" required
                                value="{{ ($patient->tracing->next_control_at) ? $patient->tracing->next_control_at->format('Y-m-d\TH:i:s') : '' }}">
                     </fieldset>
 
-                    <fieldset class="form-group col-md-3">
+                    <fieldset class="form-group col-12 col-md-5">
                         <label for="for_status">Estado del seguimiento *</label>
-                        <select name="status" id="for_status" class="form-control" required>
+                        <select name="status" id="for_status_seguimiento" class="form-control" required>
                             <option value=""></option>
                             <option value="1" {{ ($patient->tracing->status == 1) ? 'selected' : '' }}>En seguimiento
                             </option>
@@ -86,7 +87,7 @@
                         </select>
                     </fieldset>
 
-                    <fieldset class="form-group col-md-4">
+                    <fieldset class="form-group col-12 col-md-7">
                         <label for="for_establishment_id">Establecimiento que realiza seguimiento</label>
                         <select name="establishment_id" id="for_establishment_id" class="form-control">
                             @foreach($establishments as $estab)
@@ -95,21 +96,20 @@
                             @endforeach
                         </select>
                     </fieldset>
-
-                    <fieldset class="form-group col-6 col-md-1">
-                        <label for="for_functionary">Func. Salud</label>
-                        <select name="functionary" id="for_functionary" class="form-control">
-                            <option value=""></option>
-                            <option value="1" {{ ($patient->tracing->functionary === 1) ? 'selected' : '' }}>Si</option>
-                            <option value="0" {{ ($patient->tracing->functionary === 0) ? 'selected' : '' }}>No</option>
-                        </select>
-                    </fieldset>
-
-
                 </div>
 
+<!--**********************************-->
                 <div class="form-row">
-                    <fieldset class="form-group col-md-1">
+                  <fieldset class="form-group col-6 col-sm-6 col-md-3">
+                      <label for="for_functionary">Func. Salud</label>
+                      <select name="functionary" id="for_functionary" class="form-control">
+                          <option value=""></option>
+                          <option value="1" {{ ($patient->tracing->functionary === 1) ? 'selected' : '' }}>Si</option>
+                          <option value="0" {{ ($patient->tracing->functionary === 0) ? 'selected' : '' }}>No</option>
+                      </select>
+                  </fieldset>
+
+                    <fieldset class="form-group col-6 col-sm-6 col-md-3">
                         <label for="for_symptoms">Síntomas *</label>
                         <select name="symptoms" id="for_symptoms" class="form-control" required>
                             <option value=""></option>
@@ -118,56 +118,55 @@
                         </select>
                     </fieldset>
 
-
-                    <fieldset class="form-group col-md-3">
+                    <fieldset class="form-group col-12 col-sm-6 col-md-3">
                         <label for="for_symptoms_start_at">Inicio de síntomas</label>
                         <input type="datetime-local" class="form-control" name="symptoms_start_at"
                                id="for_symptoms_start_at"
                                value="{{ ($patient->tracing->symptoms_start_at) ? $patient->tracing->symptoms_start_at->format('Y-m-d\TH:i:s') : '' }}">
                     </fieldset>
 
-                    <fieldset class="form-group col-md-3">
+                    <fieldset class="form-group col-12 col-sm-6 col-md-3">
                         <label for="for_symptoms_end_at">Fin de síntomas</label>
                         <input type="datetime-local" class="form-control" name="symptoms_end_at"
                                id="for_symptoms_end_at"
                                value="{{ ($patient->tracing->symptoms_end_at) ? $patient->tracing->symptoms_end_at->format('Y-m-d\TH:i:s') : '' }}">
                     </fieldset>
                 </div>
+<!--**********************************-->
 
                 <div class="form-row">
-                    <fieldset class="form-group col-md-2">
+                    <fieldset class="form-group col-12 col-sm-6 col-md-4">
                         <label for="for_quarantine_start_at">Inicio Cuarentena *</label>
                         <input type="date" class="form-control" name="quarantine_start_at"
                                id="for_quarantine_start_at" required
                                value="{{ ($patient->tracing->quarantine_start_at) ? $patient->tracing->quarantine_start_at->format('Y-m-d') : '' }}">
                     </fieldset>
 
-                    <fieldset class="form-group col-md-2">
+                    <fieldset class="form-group col-12 col-sm-6 col-md-4">
                         <label for="for_quarantine_end_at">Término de Cuarentena *</label>
                         <input type="date" class="form-control" name="quarantine_end_at"
                                id="for_quarantine_end_at" required
                                value="{{ ($patient->tracing->quarantine_end_at) ? $patient->tracing->quarantine_end_at->format('Y-m-d') : '' }}">
                     </fieldset>
 
-                    <fieldset class="form-group col-md-5">
+                    <fieldset class="form-group col-12 col-sm-12 col-md-4">
                         <label for="for_cannot_quarantine">No puede realizar cuarentena</label>
                         <input type="text" class="form-control" name="cannot_quarantine"
                                id="for_cannot_quarantine" value="{{ $patient->tracing->cannot_quarantine }}">
                     </fieldset>
-
-
                 </div>
 
-                <div class="form-row">
+<!--**********************************-->
 
-                    <fieldset class="form-group col-md-6">
+                <div class="form-row">
+                    <fieldset class="form-group col-12 col-sm-6 col-md-4">
                         <label for="for_responsible_family_member">Familiar responsable / teléfono</label>
                         <input type="text" class="form-control" name="responsible_family_member"
                                id="for_responsible_family_member"
                                value="{{ $patient->tracing->responsible_family_member }}">
                     </fieldset>
 
-                    <fieldset class="form-group col-md-2">
+                    <fieldset class="form-group col-12 col-sm-6 col-md-3">
                         <label for="for_prevision">Previsión</label>
                         <select name="prevision" id="for_prevision" class="form-control">
                             <option value=""></option>
@@ -195,7 +194,7 @@
                         </select>
                     </fieldset>
 
-                    <fieldset class="form-group col-6 col-md-1">
+                    <fieldset class="form-group col-12 col-sm-6 col-md-2">
                         <label for="for_gestation">Gestante *</label>
                         <select name="gestation" id="for_gestation" class="form-control" required>
                             <option value=""></option>
@@ -204,44 +203,40 @@
                         </select>
                     </fieldset>
 
-                    <fieldset class="form-group col-6 col-md-2">
+                    <fieldset class="form-group col-12 col-sm-6 col-md-3">
                         <label for="for_gestation_week">Semanas de gestación</label>
                         <input type="text" class="form-control" name="gestation_week"
                                id="for_gestation_week" value="{{ $patient->tracing->gestation_week }}">
                     </fieldset>
 
                 </div>
-
+<!--**********************************-->
                 <div class="form-row">
-                    <fieldset class="form-group col">
+                    <fieldset class="form-group col-12 col-sm-6">
                         <label for="for_allergies">Alergias</label>
                         <input type="text" class="form-control" name="allergies"
                                id="for_allergies" value="{{ $patient->tracing->allergies }}">
                     </fieldset>
 
-                    <fieldset class="form-group col">
+                    <fieldset class="form-group col-12 col-sm-6">
                         <label for="for_common_use_drugs">Farmacos de uso común</label>
                         <input type="text" class="form-control" name="common_use_drugs"
                                id="for_common_use_drugs" value="{{ $patient->tracing->common_use_drugs }}">
                     </fieldset>
-                </div>
 
-                <div class="form-row">
-                    <fieldset class="form-group col">
+                    <fieldset class="form-group col-12 col-sm-6">
                         <label for="for_morbid_history">Antecedentes Mórbidos</label>
                         <input type="text" class="form-control" name="morbid_history"
                                id="for_morbid_history" value="{{ $patient->tracing->morbid_history }}">
                     </fieldset>
-                </div>
 
-                <div class="form-row">
-                    <fieldset class="form-group col">
+                    <fieldset class="form-group col-12 col-sm-6">
                         <label for="for_family_history">Antecedentes Familiares</label>
                         <input type="text" class="form-control" name="family_history"
                                id="for_family_history" value="{{ $patient->tracing->family_history }}">
                     </fieldset>
                 </div>
-
+<!--**********************************-->
                 <div class="form-row">
 
                 <!-- <fieldset class="form-group col-6 col-md-1">
@@ -280,24 +275,24 @@
                 </select>
             </fieldset> -->
                 </div>
-
+<!--**********************************-->
                 <div class="form-row">
 
-                    <fieldset class="form-group col">
+                    <fieldset class="form-group col-12 col-sm-6">
                         <label for="for_indications">Indicaciones</label>
                         <textarea class="form-control" name="indications"
                                   id="for_indications" rows="4">{{ $patient->tracing->indications }}</textarea>
                     </fieldset>
 
-                    <fieldset class="form-group col">
+                    <fieldset class="form-group col-12 col-sm-6">
                         <label for="for_observations">Observaciones</label>
                         <textarea class="form-control" name="observations"
                                   id="for_observations" rows="4">{{ $patient->tracing->observations }}</textarea>
                     </fieldset>
 
                 </div>
-
-                <button type="submit" class="btn btn-primary">Guardar</button>
+<!--**********************************-->
+                <button type="submit" class="btn btn-primary ">Guardar</button>
 
             </form>
 
