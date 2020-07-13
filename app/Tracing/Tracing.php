64dee3e0e39f6f93fac095052d6503e42c396c26
sick <sick_iqq@hotmail.com>
2020-07-13 14:29:26 -0600

    funcion getSymptoms: update

--- app/Tracing/Tracing.php
+++ app/Tracing/Tracing.php
@@ -119,9 +119,39 @@ class Tracing extends Model  implements Auditable
         $symptoms = str_replace("Odinofagia", "Dolor de garganta", $symptoms);
         $symptoms = str_replace("Mialgias", "Dolor muscular", $symptoms);
         //se genera array resultado
+        $resultado = explode(",", $symptoms);
         $resultado = array_unique(explode(",", $symptoms));
-        return $resultado;
+        $resultado = array_map('trim', $resultado);
+        //ciclo obtener array
+        $array_final = array();
+        $array_otros = array();
+        foreach ($resultado as $key => $result) {
+            if ($result == "Anosmia" || $result == "Ageusia" || $result == "Dolor toráxico" || $result == "Calofrios" || $result == "Otro") {
+                array_push($array_otros, $result);
+            }else{
+                array_push($array_final, $result);
+            }
+        }
+        array_push($array_final, $array_otros);
+        return $array_final;
     }
+
+    // fiebre, tos, dificultad respiratoria, dolor muscular, dolor de garganta, dolor de cabeza, diarrea, otro
+    //
+    // "1"	"Fiebre"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    // "2"	"Tos"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    // "11"	"Dificultad para respirar"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    // "3"	"Mialgias"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    // "4"	"Odinofagia"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    // "10"	"Cefalea"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    // "8"	"Diarrea"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    //
+    // "5"	"Anosmia"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    // "6"	"Ageusia"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    // "7"	"Dolor toráxico"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    // "9"	"Calofrios"	"2020-06-17 15:19:52"	"2020-06-17 15:19:52"	\N
+    // "12"	"Otro"	"2020-06-18 23:27:07"	"2020-06-18 23:27:08"	\N
+
 
     protected static function booted()
     {
