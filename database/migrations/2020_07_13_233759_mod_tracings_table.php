eb4857e9e6a61f17541b2b335ea818ffbff4dc0e
AquaroTorres <alvarotorres@gmail.com>
2020-07-15 14:31:32 -0400

    Risk Rating en tracing

--- database/migrations/2020_07_13_233759_mod_tracings_table.php
+++ database/migrations/2020_07_13_233759_mod_tracings_table.php
@@ -14,20 +14,17 @@ class ModTracingsTable extends Migration
     public function up()
     {
         Schema::table('tracings', function (Blueprint $table) {
-            //
-            //primero drop de los campos antiguos
+            /* Primero drop de los campos antiguos */
             $table->dropColumn('help_basket');
             $table->dropColumn('psychological_intervention');
             $table->dropColumn('requires_hospitalization');
             $table->dropColumn('requires_licence');
-            
 
+            /* Nuevos campos */
+            $table->unsignedSmallInteger('risk_rating')->after('status')->nullable();
             $table->string('employer_name')->after('observations')->nullable();
             $table->date('last_day_worked')->after('employer_name')->nullable();
             $table->string('employer_contact')->after('last_day_worked')->nullable();
-
-
-            
         });
     }
 
@@ -39,13 +36,14 @@ class ModTracingsTable extends Migration
     public function down()
     {
         Schema::table('tracings', function (Blueprint $table) {
-            //
-            //se crean las 4 columnas eliminadas
+            /* Se crean las 4 columnas eliminadas */
             $table->boolean('help_basket')->nullable();
             $table->boolean('psychological_intervention')->nullable();
             $table->boolean('requires_hospitalization')->nullable();
             $table->boolean('requires_licence')->nullable();
 
+            /* Eliminar las nuevas */
+            $table->dropColumn('risk_rating');
             $table->dropColumn('employer_name');
             $table->dropColumn('last_day_worked');
             $table->dropColumn('employer_contact');
