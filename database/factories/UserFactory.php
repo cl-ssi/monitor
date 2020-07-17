ab20b6ffe6e7f6b8aadfd60b436ed46820961052
AquaroTorres <alvarotorres@gmail.com>
2020-07-17 13:30:17 -0400

    Pacientes notificados

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
+        'run' => $faker->number,
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
--- routes/web.php
+++ routes/web.php
@@ -81,6 +81,7 @@ Route::prefix('patients')->name('patients.')->middleware('auth')->group(function
     });
 
     Route::prefix('tracings')->name('tracings.')->middleware('auth')->group(function () {
+        Route::get('/notifications', 'TracingController@notificationsReport')->name('notifications_report');
         Route::get('/cartoindex', 'TracingController@carToIndex')->name('cartoindex');
         Route::get('/reportbycommune', 'TracingController@reportByCommune')->name('reportbycommune');
         Route::get('/mapbycommunes', 'TracingController@mapByCommune')->name('mapbycommunes');
