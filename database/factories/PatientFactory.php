227da0d74d6c2427a278c3c08913bb3a89f7a03c
AquaroTorres <alvarotorres@gmail.com>
2020-07-21 11:18:57 -0400

    Factory Patient

--- database/factories/PatientFactory.php
+++ database/factories/PatientFactory.php
@@ -5,21 +5,19 @@
 use App\Patient;
 use Faker\Generator as Faker;
 
+//$faker = Faker::create('es_SP');
+
 $factory->define(Patient::class, function (Faker $faker) {
     return [
-                //'id' => 2,
-//                'id', 'run', 'dv', 'other_identification', 'name', 'fathers_family',
-//                'mothers_family', 'gender', 'birthday', 'status', 'deceased_at'
-//
-                'run' => "11418555",
-                'dv' => '3',
-                'other_identification' => "8786554",
-                'name' => $faker->name(),
-                'fathers_family' => $faker->lastName(),
-                'mothers_family' => $faker->lastName(),
-                'gender' => "male",
-                'birthday' => null,
-                'status' => "cuarentena---",
-                'deceased_at' => null
+        'run' => $faker->numberBetween($min = 1000000, $max = 26000000),
+        'dv' => $faker->randomElement(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'k']),
+        'other_identification' => "8786554",
+        'name' => $faker->name(),
+        'fathers_family' => $faker->lastName(),
+        'mothers_family' => $faker->lastName(),
+        'gender' => $faker->randomElement(['male', 'female']),
+        'birthday' => $faker->dateTimeBetween('-95 years', '-1 days'),
+        'status' => "Ambulatorio",
+        'deceased_at' => null
     ];
 });
