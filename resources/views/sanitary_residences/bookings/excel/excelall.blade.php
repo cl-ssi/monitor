739f0e9a05134bc8baee9e4b1aedcde9f5bd7913
Esteban <tebiccr@gmail.com>
2020-08-08 01:28:44 -0400

    dd

--- resources/views/sanitary_residences/bookings/excel/excelall.blade.php
+++ resources/views/sanitary_residences/bookings/excel/excelall.blade.php
@@ -63,12 +63,8 @@
             <td nowrap>{{$booking->room->residence->name}}</td>
             <td nowrap>{{$booking->room->floor}}</td>
             <td nowrap>{{$booking->room->number}}</td>
-            <td nowrap>{{ ($booking->patient->demographic)?$booking->patient->demographic->commune->name:'' }}</td>
-            @if(is_null($booking->patient->suspectCases->last()->deleted_at))
-            <td nowrap>{{ ($booking->patient->suspectCases->last())? $booking->patient->suspectCases->last()->establishment->alias.' - '.$booking->patient->suspectCases->last()->origin:'' }}</td>            
-            @else
-            <td nowrap> ES EL BORRADO </td>            
-            @endif
+            <td nowrap>{{ ($booking->patient->demographic)?$booking->patient->demographic->commune->name:'' }}</td>            
+            <td nowrap>{{ ($booking->patient->suspectCases->last())? dd($booking->patient->suspectCases->last()->establishment->alias).' - '.$booking->patient->suspectCases->last()->origin:'' }}</td>            
             <td nowrap> {{$booking->doctor}} </td>
             <td nowrap>{{$booking->patient->identifier}}</td>
             <td nowrap>{{$booking->patient->fathers_family}} {{$booking->patient->mothers_family}}</td>
