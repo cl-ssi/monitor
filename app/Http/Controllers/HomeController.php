a94ee8a69dc7cb0b0771be1312b31e2433b62c8d
Esteban <tebiccr@gmail.com>
2020-08-22 14:33:27 -0400

    fixed #41 :happy:

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\EstablishmentUser;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $establishmentsusers = EstablishmentUser::where('user_id',Auth::id())->get();
+        return view('home',compact('establishmentsusers'));
     }
 }
