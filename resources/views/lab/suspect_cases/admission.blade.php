00050da4f36c12fc6091d8edb0deb45316540dfa
Esteban <tebiccr@gmail.com>
2020-09-15 12:34:58 -0300

    calcula edad a partir de la fecha ingresada

--- resources/views/lab/suspect_cases/admission.blade.php
+++ resources/views/lab/suspect_cases/admission.blade.php
@@ -493,6 +493,29 @@ $(document).ready(function(){
         $("#for_run").removeAttr('readonly', 'readonly');
     })
 
+    $("#for_birthday").change(function () {        
+        var birthDate =document.getElementById('for_birthday').value;
+        var d = new Date(birthDate);
+
+        var mdate = birthDate.toString();
+        var yearThen = parseInt(mdate.substring(0,4), 10);
+        var monthThen = parseInt(mdate.substring(5,7), 10);
+        var dayThen = parseInt(mdate.substring(8,10), 10);
+        
+        var today = new Date();
+        var birthday = new Date(yearThen, monthThen-1, dayThen);
+     
+        var differenceInMilisecond = today.valueOf() - birthday.valueOf();
+      
+        
+        var year_age = Math.floor(differenceInMilisecond / 31536000000);
+
+        $("#for_age").val(year_age);
+        
+
+        
+    })
+
 });
 
 
