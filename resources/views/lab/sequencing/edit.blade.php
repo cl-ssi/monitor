0ab4c6b1c0bb75c864562bbac8a0dabbfab405f1
tebiccr <tebiccr@gmail.com>
2021-08-03 10:11:52 -0400

    fix viajero selected

--- resources/views/lab/sequencing/edit.blade.php
+++ resources/views/lab/sequencing/edit.blade.php
@@ -36,7 +36,7 @@
                 <option value="PIMS" {{ ($sequencingCriteria->critery == 'PIMS')?'selected':'' }} >PIMS</option>
                 <option value="Reinfección" {{ ($sequencingCriteria->critery == 'Reinfección')?'selected':'' }} >Reinfección</option>
                 <option value="Inusitado" {{ ($sequencingCriteria->critery == 'Inusitado')?'selected':'' }} >Inusitado</option>
-                <option value="Viajero" {{ ($sequencingCriteria->critery == 'Viajero')?'Viajero':'' }} >Viajero</option>
+                <option value="Viajero" {{ ($sequencingCriteria->critery == 'Viajero')?'selected':'' }} >Viajero</option>
             </select>
         </fieldset>
 
