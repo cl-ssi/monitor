ea78863bddbf312c1a66b1d331df8b0ee442659d
rafael1delgado <delgadorafael2011@gmail.com>
2022-06-27 20:17:34 -0400

    [feat] Add migration log_sessions

---
+++ database/migrations/2022_06_27_190033_create_log_sessions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log_sessions', function (Blueprint $table) {
+            $table->id();
+
+            $table->text('user_agent')->nullable();
+            $table->ipAddress('ip')->nullable();
+
+            $table->foreignId('user_id')->nullable()->constrained('users');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log_sessions');
+    }
+}
