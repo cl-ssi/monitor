6a5ca5d27a454b26fe3eef1f6bffa7cce6db5d17
rafael1delgado <delgadorafael2011@gmail.com>
2022-06-27 20:17:56 -0400

    [feat] Add model LogSession

---
+++ app/LogSession.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LogSession extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'log_sessions';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ip',
+        'user_agent',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
